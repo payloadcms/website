f1edd131d0c8e5d202488cbd2f6c251fd686b957
Jacob Fletcher <jacobsfletch@gmail.com>
2022-10-24 18:11:38 -0400

    chore: lints

--- components/AdminBar/index.tsx
+++ components/AdminBar/index.tsx
@@ -1,29 +1,19 @@
-import React, { useState } from 'react';
-import { PayloadMeUser, PayloadAdminBarProps, PayloadAdminBar } from 'payload-admin-bar';
-import classes from './index.module.scss';
+import React, { useState } from 'react'
+import { PayloadMeUser, PayloadAdminBarProps, PayloadAdminBar } from 'payload-admin-bar'
+import classes from './index.module.scss'
 
-export const AdminBar: React.FC<PayloadAdminBarProps & {
-  className?: string
-}> = (props) => {
-  const {
-    className,
-    collection,
-    id,
-    preview,
-    onPreviewExit
-  } = props;
+export const AdminBar: React.FC<
+  PayloadAdminBarProps & {
+    className?: string
+  }
+> = props => {
+  const { className, collection, id, preview, onPreviewExit } = props
 
-  const [user, setUser] = useState<PayloadMeUser>();
+  const [user, setUser] = useState<PayloadMeUser>()
 
   return (
-    <div
-      className={[
-        classes.adminBar,
-        user && classes.show,
-        className
-      ].filter(Boolean).join(' ')}
-    >
-      <div className={classes.blockContainer} >
+    <div className={[classes.adminBar, user && classes.show, className].filter(Boolean).join(' ')}>
+      <div className={classes.blockContainer}>
         <PayloadAdminBar
           collection={collection}
           id={id}
@@ -37,19 +27,15 @@ export const AdminBar: React.FC<PayloadAdminBarProps & {
             logo: classes.logo,
             controls: classes.controls,
           }}
-          logo={(
-            <div>
-              Payload CMS
-            </div>
-          )}
+          logo={<div>Payload CMS</div>}
           style={{
             position: 'relative',
             zIndex: 'unset',
             padding: 0,
-            backgroundColor: 'transparent'
+            backgroundColor: 'transparent',
           }}
         />
       </div>
-    </div >
+    </div>
   )
 }
