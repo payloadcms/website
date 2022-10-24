da5bc290ac7466690bd87b24bb23ba25de266702
Jacob Fletcher <jacobsfletch@gmail.com>
2022-10-24 11:26:20 -0400

    chore: modifies scss import strategy and adds webpack alias

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    const configCopy = { ...config };
+    configCopy.resolve.alias = {
+      ...config.resolve.alias,
+      '@scss': path.resolve(__dirname, './css/'),
+    }
+    return configCopy;
+  },
 }
 
 module.exports = nextConfig
