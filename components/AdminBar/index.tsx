87aeb869c6ea2ea15ec96b0909a7d52372f44c4c
Jacob Fletcher <jacobsfletch@gmail.com>
2022-10-24 11:55:17 -0400

    feat: installs payload-admin-bar #1

---
+++ components/AdminBar/index.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { PayloadMeUser, PayloadAdminBarProps, PayloadAdminBar } from 'payload-admin-bar';
+import classes from './index.module.scss';
+
+export const AdminBar: React.FC<PayloadAdminBarProps & {
+  className?: string
+}> = (props) => {
+  const {
+    className,
+    collection,
+    id,
+    preview,
+    onPreviewExit
+  } = props;
+
+  const [user, setUser] = useState<PayloadMeUser>();
+
+  return (
+    <div
+      className={[
+        classes.adminBar,
+        user && classes.show,
+        className
+      ].filter(Boolean).join(' ')}
+    >
+      <div className={classes.blockContainer} >
+        <PayloadAdminBar
+          collection={collection}
+          id={id}
+          preview={preview}
+          onPreviewExit={onPreviewExit}
+          cmsURL={process.env.NEXT_PUBLIC_CMS_URL}
+          onAuthChange={setUser}
+          className={classes.payloadAdminBar}
+          classNames={{
+            user: classes.user,
+            logo: classes.logo,
+            controls: classes.controls,
+          }}
+          logo={(
+            <div>
+              Payload CMS
+            </div>
+          )}
+          style={{
+            position: 'relative',
+            zIndex: 'unset',
+            padding: 0,
+            backgroundColor: 'transparent'
+          }}
+        />
+      </div>
+    </div >
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,33 @@
 import { AppProps } from 'next/app';
+import { AdminBar } from '../components/AdminBar';
 import { ThemeProvider } from '../components/providers/Theme';
 import '../css/app.scss';
 
-const PayloadApp = (appProps: AppProps): React.ReactElement => {
+const PayloadApp = (
+  appProps: AppProps<{
+    collection?: string;
+    id?: string;
+    preview?: boolean
+  }>
+): React.ReactElement => {
   const {
     Component,
     pageProps,
   } = appProps;
 
+  const {
+    collection,
+    id,
+    preview,
+  } = pageProps;
+
   return (
     <ThemeProvider>
+      <AdminBar 
+        id={id}
+        collection={collection}
+        preview={preview}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   )
