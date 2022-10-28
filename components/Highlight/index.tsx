1639cfbe2e0287174ca832e9e456380f7009a260
Jacob Fletcher <jacobsfletch@gmail.com>
2022-10-28 17:11:25 -0400

    feat: refines highlight animation #15

--- components/Highlight/index.tsx
+++ components/Highlight/index.tsx
@@ -18,7 +18,7 @@ export const Highlight: React.FC<{
 
   const { hasIntersected } = useIntersection({
     ref,
-    rootMargin: '50px',
+    rootMargin: '-75px',
   })
 
   const theme = useTheme()
