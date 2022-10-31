98e345586a69e2d8e8394ab312f0464c12ed1e96
Jacob Fletcher <jacobsfletch@gmail.com>
2022-10-31 13:21:01 -0400

    fix: removes App from styleguide pages

--- app/styleguide/page.tsx
+++ app/styleguide/page.tsx
@@ -1,32 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
 import { Gutter } from '../../components/Gutter'
-import { App } from '../App'
 
 const Styleguide: React.FC = () => {
   return (
-    <App>
-      <div style={{ paddingTop: 'calc(var(--header-height) + var(--base)' }}>
-        <Gutter>
-          <h1>Styleguide</h1>
-          <div>
-            <Link href="/styleguide/icons">Icons</Link>
-          </div>
-          <br />
-          <div>
-            <Link href="/styleguide/buttons">Buttons</Link>
-          </div>
-          <br />
-          <div>
-            <Link href="/styleguide/typography">Typography</Link>
-          </div>
-          <br />
-          <div>
-            <Link href="/styleguide/highlight">Highlight</Link>
-          </div>
-        </Gutter>
-      </div>
-    </App>
+    <div style={{ paddingTop: 'calc(var(--header-height) + var(--base)' }}>
+      <Gutter>
+        <h1>Styleguide</h1>
+        <div>
+          <Link href="/styleguide/icons">Icons</Link>
+        </div>
+        <br />
+        <div>
+          <Link href="/styleguide/buttons">Buttons</Link>
+        </div>
+        <br />
+        <div>
+          <Link href="/styleguide/typography">Typography</Link>
+        </div>
+        <br />
+        <div>
+          <Link href="/styleguide/highlight">Highlight</Link>
+        </div>
+      </Gutter>
+    </div>
   )
 }
 
