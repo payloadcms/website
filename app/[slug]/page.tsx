267badca3c3c50778d785c29e7e8d25c830d9c2a
James <james@trbl.design>
2022-10-31 13:17:39 -0400

    chore: removes payload admin bar for now ;(

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,18 +2,15 @@ import React from 'react'
 import { Hero } from '../../components/Hero'
 import { RenderBlocks } from '../../components/RenderBlocks'
 import { fetchPage } from '../../graphql'
-import { App } from '../App'
 
 const Page = async ({ params }) => {
-  let { slug } = params
-  if (typeof slug === 'undefined') slug = 'home'
-  const page = await fetchPage(slug)
+  const page = await fetchPage(params.slug || 'home')
 
   return (
-    <App id={page.id} collection="pages" preview={false}>
+    <React.Fragment>
       <Hero {...page.hero} />
       <RenderBlocks blocks={page.layout} />
-    </App>
+    </React.Fragment>
   )
 }
 
