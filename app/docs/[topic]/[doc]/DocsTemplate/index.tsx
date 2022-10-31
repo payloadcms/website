76092cc7eea8ebf2525437fccf08569daf40439d
James <james@trbl.design>
2022-10-31 12:31:48 -0400

    feat: fetches docs from github

--- app/docs/[topic]/[doc]/DocsTemplate/index.tsx
+++ app/docs/[topic]/[doc]/DocsTemplate/index.tsx
@@ -3,18 +3,19 @@
 import { Cell, Grid } from '@faceless-ui/css-grid'
 import React from 'react'
 import { Gutter } from '../../../../../components/Gutter'
-import { Doc } from '../data'
+import { Topic } from '../types'
 import classes from './index.module.scss'
 
-export const RenderDoc: React.FC<Doc> = ({ data }) => {
-  const { title } = data
-
+export const DocsTemplate: React.FC<{ topics: Topic[]; children: React.ReactNode }> = ({
+  // topics,
+  children,
+}) => {
   return (
     <Gutter left="half" right="half" className={classes.wrap}>
       <nav className={classes.nav}></nav>
       <Grid className={classes.grid}>
         <Cell start={2} cols={8}>
-          <h1 className={classes.title}>{title}</h1>
+          {children}
         </Cell>
       </Grid>
     </Gutter>
--- app/docs/[topic]/[doc]/page.tsx
+++ app/docs/[topic]/[doc]/page.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import { App } from '../../../App'
-import { getDoc } from './data'
-import { RenderDoc } from './Render'
+import { getDoc } from './api'
+import classes from './index.module.scss'
 
-const Doc = ({ params }) => {
+const Doc = async ({ params }) => {
   const { topic, doc } = params
 
-  const docData = getDoc({ topic, doc })
+  const {
+    data: { title },
+  } = await getDoc({ topic, doc })
 
   return (
-    <App>
-      <RenderDoc {...docData} />
-    </App>
+    <div>
+      <h1 className={classes.title}>{title}</h1>
+    </div>
   )
 }
 
