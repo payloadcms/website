899549afba4d9a583a1732a5d5b664e3d6b4a787
James <james@trbl.design>
2022-10-31 13:48:07 -0400

    chore: writes notes to determine color scheme on server

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ import '../css/app.scss'
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const { mainMenu } = await fetchGlobals()
 
+  // 1. Get cookies from headers to determine if there is a color scheme set
+  // 2. If no theme from cookies, retrieve user agent and determine what color scheme is preferred
+  // 3. Set theme on html with result
+  // 4. Pass the resulting color scheme to the Providers component and use it to instantiate state
+
   return (
     <html lang="en">
       <head>
