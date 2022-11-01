08b408494d1e3053988ef983e56092a6ccfd35fa
James <james@trbl.design>
2022-11-01 16:45:41 -0400

    chore: hero perf optimizations

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="en" data-theme="light">
       <head>
         <title>Payload CMS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
