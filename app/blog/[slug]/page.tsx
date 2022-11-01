ac86155ed206af1d40e7e63764b67758ce52d15c
Jarrod Flesch <jarrodmflesch@gmail.com>
2022-11-01 09:28:13 -0400

    feat: removes App from individual blog pages

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
-import { RenderBlogPost } from './renderPage'
-import { App } from '../../App'
+import { RenderBlogPost } from './renderBlogPost'
 import { fetchBlogPost } from '../../../graphql'
 
 const Page = async ({ params }) => {
   const { slug } = params
   const blogPost = await fetchBlogPost(slug)
 
-  return (
-    <App>
-      <RenderBlogPost {...blogPost} />
-    </App>
-  )
+  return <RenderBlogPost {...blogPost} />
 }
 
 export default Page
