a4e9e0258999224751eddf05a50f74ac29ab21c5
James <james@trbl.design>
2022-11-01 13:22:31 -0400

    chore: removes cookie usage

--- app/docs/[topic]/[doc]/layout.tsx
+++ app/docs/[topic]/[doc]/layout.tsx
@@ -1,15 +1,12 @@
-import { cookies } from 'next/headers'
 import React from 'react'
 import { getTopics } from './api'
 import { DocsTemplate } from './DocsTemplate'
-import { openTopicsCookieName } from './shared'
 
 const Layout = async ({ params, children }) => {
   const topics = await getTopics()
-  const openTopics = JSON.parse(cookies().get(openTopicsCookieName)?.value || '[]')
 
   return (
-    <DocsTemplate {...{ topics, openTopics, doc: params.doc, topic: params.topic }}>
+    <DocsTemplate {...{ topics, openTopics: [], doc: params.doc, topic: params.topic }}>
       {children}
     </DocsTemplate>
   )
