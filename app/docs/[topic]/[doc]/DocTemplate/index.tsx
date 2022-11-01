491c4b97bd290e2b4aa1ee33d7dba488a8487d58
James <james@trbl.design>
2022-11-01 11:25:44 -0400

    chore: styles links

--- app/docs/[topic]/[doc]/DocTemplate/index.tsx
+++ app/docs/[topic]/[doc]/DocTemplate/index.tsx
@@ -7,9 +7,9 @@ import classes from './index.module.scss'
 
 export const DocTemplate: React.FC<{ title: string; content: any }> = ({ title, content }) => {
   return (
-    <React.Fragment>
+    <div className={classes.doc}>
       <h1 className={classes.title}>{title}</h1>
       <MDXRemote {...content} components={components} />
-    </React.Fragment>
+    </div>
   )
 }
