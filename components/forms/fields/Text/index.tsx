7f5b0b656eccd20c94cf5215850c09a376126e72
Jacob Fletcher <jacobsfletch@gmail.com>
2022-11-01 22:40:48 -0400

    feat: useField hook

--- components/forms/fields/Text/index.tsx
+++ components/forms/fields/Text/index.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import React, { useCallback, useEffect, useState } from 'react'
-import { useFormField } from '../../useFormField'
+import React from 'react'
 import Error from '../../Error'
 import Label from '../../Label'
 import { Validate } from '../../types'
+import { FieldProps } from '../types'
+import { useField } from '../useField'
 
 import classes from './index.module.scss'
 
@@ -19,17 +20,11 @@ const defaultValidate: Validate = val => {
   return 'Please enter a value.'
 }
 
-export const Text: React.FC<{
-  path?: string
-  required?: boolean
-  validate?: Validate
-  label?: string
-  placeholder?: string
-  type?: 'text' | 'hidden'
-  onChange?: (value: string) => void // eslint-disable-line no-unused-vars
-  initialValue?: string
-  className?: string
-}> = props => {
+export const Text: React.FC<
+  FieldProps<string> & {
+    type?: 'text' | 'hidden'
+  }
+> = props => {
   const {
     path,
     required = false,
@@ -37,42 +32,18 @@ export const Text: React.FC<{
     label,
     placeholder,
     type = 'text',
-    onChange,
-    initialValue: initialValueFromProps, // allow external control
+    onChange: onChangeFromProps,
+    initialValue,
     className,
   } = props
 
-  const fieldFromContext = useFormField<string>({
+  const { onChange, value, showError, errorMessage } = useField<string>({
+    initialValue,
+    onChange: onChangeFromProps,
     path,
-    validate: required ? validate : undefined,
+    validate,
+    required,
   })
-
-  const { value: valueFromContext, showError, setValue, errorMessage } = fieldFromContext
-
-  const valueFromContextOrProps = valueFromContext || initialValueFromProps
-
-  const [internalState, setInternalState] = useState<string>(
-    valueFromContext || initialValueFromProps,
-  ) // not debounced
-
-  useEffect(() => {
-    if (valueFromContextOrProps !== undefined && valueFromContextOrProps !== internalState)
-      setInternalState(valueFromContextOrProps)
-  }, [valueFromContextOrProps, internalState])
-
-  const handleChange = useCallback(
-    e => {
-      const { value: incomingValue } = e.target
-      setInternalState(incomingValue)
-      if (typeof setValue === 'function') {
-        setValue(incomingValue)
-      }
-      if (typeof onChange === 'function') {
-        onChange(incomingValue)
-      }
-    },
-    [onChange, setValue],
-  )
 
   return (
     <div className={[className, classes.wrap].filter(Boolean).join(' ')}>
@@ -80,8 +51,10 @@ export const Text: React.FC<{
       <Label htmlFor={path} label={label} required={required} />
       <input
         className={classes.input}
-        value={(internalState as string) || ''}
-        onChange={handleChange}
+        value={value || ''}
+        onChange={e => {
+          onChange(e.target.value)
+        }}
         placeholder={placeholder}
         type={type}
         id={path}
--- components/forms/fields/Textarea/index.tsx
+++ components/forms/fields/Textarea/index.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import React, { useCallback, useEffect, useState } from 'react'
-import { useFormField } from '../../useFormField'
+import React from 'react'
 import Error from '../../Error'
 import Label from '../../Label'
 import { Validate } from '../../types'
 
 import classes from './index.module.scss'
+import { useField } from '../useField'
+import { FieldProps } from '../types'
 
 const defaultValidate: Validate = val => {
   const stringVal = val as string
@@ -19,60 +20,30 @@ const defaultValidate: Validate = val => {
   return 'Please enter a value.'
 }
 
-export const Textarea: React.FC<{
-  path?: string
-  required?: boolean
-  validate?: Validate
-  label?: string
-  placeholder?: string
-  onChange?: (value: string) => void // eslint-disable-line no-unused-vars
-  marginBottom?: boolean
-  rows?: number
-  initialValue?: string
-  className?: string
-}> = props => {
+export const Textarea: React.FC<
+  FieldProps<string> & {
+    rows?: number
+  }
+> = props => {
   const {
     path,
     required = false,
     validate = defaultValidate,
     label,
     placeholder,
-    onChange,
+    onChange: onChangeFromProps,
     rows = 3,
-    initialValue: initialValueFromProps, // allow external control
+    initialValue,
     className,
   } = props
 
-  const fieldFromContext = useFormField<string>({
+  const { onChange, value, showError, errorMessage } = useField<string>({
+    initialValue,
+    onChange: onChangeFromProps,
     path,
-    validate: required ? validate : undefined,
+    validate,
+    required,
   })
-
-  const { value: valueFromContext, showError, setValue, errorMessage } = fieldFromContext
-
-  const valueFromContextOrProps = valueFromContext || initialValueFromProps
-  const [internalState, setInternalState] = useState<string>(
-    valueFromContext || initialValueFromProps,
-  ) // not debounced
-
-  useEffect(() => {
-    if (valueFromContextOrProps !== undefined && valueFromContextOrProps !== internalState)
-      setInternalState(valueFromContextOrProps)
-  }, [valueFromContextOrProps, internalState])
-
-  const handleChange = useCallback(
-    e => {
-      const { value: incomingValue } = e.target
-      setInternalState(incomingValue)
-      if (typeof setValue === 'function') {
-        setValue(incomingValue)
-      }
-      if (typeof onChange === 'function') {
-        onChange(incomingValue)
-      }
-    },
-    [onChange, setValue],
-  )
 
   return (
     <div className={[className, classes.wrap].filter(Boolean).join(' ')}>
@@ -81,8 +52,10 @@ export const Textarea: React.FC<{
       <textarea
         rows={rows}
         className={classes.textarea}
-        value={(internalState as string) || ''}
-        onChange={handleChange}
+        value={value || ''}
+        onChange={e => {
+          onChange(e.target.value)
+        }}
         placeholder={placeholder}
         id={path}
         name={path}
