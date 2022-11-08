59c07117867f2ed7f28bb1a76361089b4f9f2ced
Jarrod Flesch <jarrodmflesch@gmail.com>
2022-11-07 18:02:48 -0800

    fix: adjusts headerObserver on defaultHero

--- app/case-studies/[slug]/renderLayout.tsx
+++ app/case-studies/[slug]/renderLayout.tsx
@@ -6,5 +6,9 @@ import { useTheme } from '@components/providers/Theme'
 export const RenderLayout = ({ children }) => {
   const theme = useTheme()
 
-  return <HeaderObserver color={theme}>{children}</HeaderObserver>
+  return (
+    <HeaderObserver color={theme} pullUp>
+      {children}
+    </HeaderObserver>
+  )
 }
