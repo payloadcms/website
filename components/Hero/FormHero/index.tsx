07ce934ac50262fc4a6b05acbe6cd57d393fea4e
Jacob Fletcher <jacobsfletch@gmail.com>
2022-11-08 11:05:58 -0800

    chore: refines FormHero

--- components/Hero/FormHero/index.tsx
+++ components/Hero/FormHero/index.tsx
@@ -9,6 +9,7 @@ import { CMSForm } from '@components/CMSForm'
 import { Page } from '@root/payload-types'
 import { PixelBackground } from '@components/PixelBackground'
 import { CheckmarkIcon } from '@components/graphics/CheckmarkIcon'
+import { HeaderObserver } from '@components/HeaderObserver'
 import classes from './index.module.scss'
 
 export type FormHeroProps = Page['hero']
@@ -17,56 +18,58 @@ export const FormHero: React.FC<FormHeroProps> = props => {
   const { richText, form } = props
 
   return (
-    <ThemeProvider theme="dark">
-      <div className={classes.formHero}>
-        <div className={classes.bgWrapper}>
-          <Gutter disableMobile className={classes.bgGutter}>
-            <div className={classes.bg1}>
-              <div className={classes.pixelBG}>
-                <PixelBackground />
+    <HeaderObserver color="light" pullUp>
+      <ThemeProvider theme="dark">
+        <div className={classes.formHero}>
+          <div className={classes.bgWrapper}>
+            <Gutter disableMobile className={classes.bgGutter}>
+              <div className={classes.bg1}>
+                <div className={classes.pixelBG}>
+                  <PixelBackground />
+                </div>
               </div>
-            </div>
-          </Gutter>
-        </div>
-        <div className={classes.bg2Wrapper}>
-          <Gutter className={classes.bgGutter}>
-            <Grid className={classes.bg2Grid}>
-              <Cell start={7} cols={6} startM={2} colsM={7} className={classes.bg2Cell}>
-                <div className={classes.bg2} />
+            </Gutter>
+          </div>
+          <div className={classes.bg2Wrapper}>
+            <Gutter className={classes.bgGutter}>
+              <Grid className={classes.bg2Grid}>
+                <Cell start={7} cols={6} startM={2} colsM={7} className={classes.bg2Cell}>
+                  <div className={classes.bg2} />
+                </Cell>
+              </Grid>
+            </Gutter>
+          </div>
+          <Gutter className={classes.gutter}>
+            <Grid>
+              <Cell cols={6} colsM={8} startM={1} className={classes.richTextCell}>
+                {richText && (
+                  <RichText
+                    className={classes.richText}
+                    content={richText}
+                    customRenderers={{
+                      li: ({ node: { children }, Serialize, index }) => {
+                        return (
+                          <li key={`list-item-${index}`} className={classes.li}>
+                            <div className={classes.bullet}>
+                              <CheckmarkIcon />
+                            </div>
+                            <Serialize content={children} />
+                          </li>
+                        )
+                      },
+                    }}
+                  />
+                )}
+              </Cell>
+              <Cell cols={6} start={8} colsM={8} startM={1} className={classes.formCell}>
+                <div className={classes.formCellContent}>
+                  <CMSForm form={form} />
+                </div>
               </Cell>
             </Grid>
           </Gutter>
         </div>
-        <Gutter className={classes.gutter}>
-          <Grid>
-            <Cell cols={5} startL={2} colsM={8} startM={1} className={classes.richTextCell}>
-              {richText && (
-                <RichText
-                  className={classes.richText}
-                  content={richText}
-                  customRenderers={{
-                    li: ({ node: { children }, Serialize, index }) => {
-                      return (
-                        <li key={`list-item-${index}`} className={classes.li}>
-                          <div className={classes.bullet}>
-                            <CheckmarkIcon />
-                          </div>
-                          <Serialize content={children} />
-                        </li>
-                      )
-                    },
-                  }}
-                />
-              )}
-            </Cell>
-            <Cell cols={6} start={8} colsL={4} colsM={8} startM={1} className={classes.formCell}>
-              <div className={classes.formCellContent}>
-                <CMSForm form={form} />
-              </div>
-            </Cell>
-          </Grid>
-        </Gutter>
-      </div>
-    </ThemeProvider>
+      </ThemeProvider>
+    </HeaderObserver>
   )
 }
