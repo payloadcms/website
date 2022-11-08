e65fea228d91f6151fedfd272a98d97ad9cbd21b
Jacob Fletcher <jacobsfletch@gmail.com>
2022-11-08 11:52:50 -0800

    feat: renders hr and links in CardGrid block

--- components/blocks/CardGrid/index.tsx
+++ components/blocks/CardGrid/index.tsx
@@ -4,6 +4,7 @@ import { RichText } from '@components/RichText'
 import { Cell, Grid } from '@faceless-ui/css-grid'
 import React from 'react'
 import { SquareCard } from '@components/Cards/SquareCard'
+import { Button } from '@components/Button'
 import { Page } from '../../../payload-types'
 import { Gutter } from '../../Gutter'
 import classes from './index.module.scss'
@@ -12,15 +13,39 @@ export type CardGridProps = Extract<Page['layout'][0], { blockType: 'cardGrid' }
 
 export const CardGrid: React.FC<CardGridProps> = props => {
   const {
-    cardGridFields: { richText, cards },
+    cardGridFields: { richText, cards, links },
   } = props
 
   const hasCards = Array.isArray(cards) && cards.length > 0
+  const hasLinks = Array.isArray(links) && links.length > 0
 
   return (
     <BlockSpacing className={classes.cardGrid}>
       <Gutter>
-        {richText && <RichText className={classes.richText} content={richText} />}
+        <hr className={classes.hr} />
+        <Grid className={classes.intro}>
+          {richText && (
+            <Cell cols={8} colsM={8}>
+              <RichText className={classes.richText} content={richText} />
+            </Cell>
+          )}
+          {hasLinks && (
+            <Cell cols={3} colsL={4} start={10} startL={9} startM={1} colsM={8}>
+              {links.map(({ link }, index) => {
+                return (
+                  <Button
+                    reference={link.reference}
+                    key={index}
+                    appearance="default"
+                    icon="arrow"
+                    label={link.label}
+                    fullWidth
+                  />
+                )
+              })}
+            </Cell>
+          )}
+        </Grid>
         {hasCards && (
           <div className={classes.cards}>
             <div className={classes.bg}>
