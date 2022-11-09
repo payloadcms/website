a60a3466e847fd5933769129362b9f9d7971a365
James <james@trbl.design>
2022-11-09 13:38:44 -0800

    chore: css improvements to code block

--- src/blocks/CodeBlock/index.tsx
+++ src/blocks/CodeBlock/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Code from '@components/Code'
 import { Gutter } from '@components/Gutter'
 import { ReusableContent } from '@root/payload-types'
+import classes from './index.module.scss'
 
 type Props = Extract<ReusableContent['layout'][0], { blockType: 'code' }>
 
@@ -15,8 +16,17 @@ export const CodeBlock: React.FC<Props> = ({ codeFields }) => {
   return (
     <Gutter>
       <Grid>
-        <Cell start={3} cols={8} startM={2} colsM={6} startS={1} colsS={8}>
-          <Code>{code}</Code>
+        <Cell
+          start={3}
+          cols={8}
+          startM={2}
+          colsM={6}
+          startS={1}
+          colsS={8}
+          className={classes.codeBlock}
+        >
+          <Code>{`${code}
+          `}</Code>
         </Cell>
       </Grid>
     </Gutter>
