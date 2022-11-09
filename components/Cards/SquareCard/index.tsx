5836fae28820873d2c6ed3a561062c69f0cdcf79
Jacob Fletcher <jacobsfletch@gmail.com>
2022-11-09 10:36:59 -0800

    chore: refines card hover

--- components/Cards/SquareCard/index.tsx
+++ components/Cards/SquareCard/index.tsx
@@ -8,8 +8,12 @@ import classes from './index.module.scss'
 export const SquareCard: React.FC<SquareCardProps> = props => {
   const { title, className, leader, description } = props
 
+  const hasLink = props.link.url || props.link.reference
+
   return (
-    <div className={[className, classes.card].filter(Boolean).join(' ')}>
+    <div
+      className={[className, classes.card, !hasLink && classes.noLink].filter(Boolean).join(' ')}
+    >
       <CMSLink className={classes.link} {...props.link}>
         <div className={classes.bg} />
         {leader && <span className={classes.leader}>{leader}</span>}
