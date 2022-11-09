3f0140b46f1063413b62a1a194d06824a1f3f788
James <james@trbl.design>
2022-11-09 15:56:33 -0800

    chore: adjusts setTheme to allow system default

--- src/providers/Theme/shared.ts
+++ src/providers/Theme/shared.ts
@@ -1,1 +1,17 @@
+import type { Theme } from './types'
+
 export const themeLocalStorageKey = 'payload-theme'
+
+export const defaultTheme = 'light'
+
+export const getImplicitPreference = (): Theme => {
+  const mediaQuery = '(prefers-color-scheme: dark)'
+  const mql = window.matchMedia(mediaQuery)
+  const hasImplicitPreference = typeof mql.matches === 'boolean'
+
+  if (hasImplicitPreference) {
+    return mql.matches ? 'dark' : 'light'
+  }
+
+  return undefined
+}
