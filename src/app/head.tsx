e00d030bf1e9b15c4b0bf6eab2a13c64deb62fc6
Jarrod Flesch <jarrodmflesch@gmail.com>
2022-11-10 16:27:15 -0800

    feat: analytics

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,16 +1,6 @@
-import Meta from '@components/Meta'
-import { fetchPage } from '@graphql'
+import React from 'react'
+import { GoogleAnalytics } from '@components/Analytics/GoogleAnalytics'
 
-export default async ({ params }) => {
-  const { slug } = params
-  const { meta } = await fetchPage(slug)
-
-  return (
-    <Meta
-      title={meta?.title}
-      description={meta?.description}
-      image={meta?.image}
-      slug={`/${slug ? slug.join('/') : ''}`}
-    />
-  )
+export default async () => {
+  return <GoogleAnalytics />
 }
