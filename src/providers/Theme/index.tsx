6f3caf03011a3cffccff4ba25bad4ba58ec5c5f0
James <james@trbl.design>
2022-11-10 16:52:45 -0800

    chore: adds log for testing

--- src/providers/Theme/index.tsx
+++ src/providers/Theme/index.tsx
@@ -10,6 +10,7 @@ export const ThemeProvider: React.FC<{
   children: React.ReactNode
   className?: string
 }> = ({ theme, children, className }) => {
+  console.log({ theme })
   return (
     <ThemeContext.Provider value={theme}>
       <div
