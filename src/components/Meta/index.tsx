53fb89d8dc7ca825003b831e1bde20739e5b1305
James <james@trbl.design>
2022-11-13 20:01:52 -0500

    chore: fixes og:url

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -37,7 +37,7 @@ const Meta: React.FC<MetaType> = ({
       <meta name="description" content={description} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
-      <meta property="og:url" content={`https://cms.payloadcms.com/${slug}`} />
+      <meta property="og:url" content={`https://payloadcms.com/${slug}`} />
       <meta property="og:site_name" content="Payload CMS" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="og:type" content="website" />
