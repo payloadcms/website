d8ae4e18e6061ccb0dce4e16d9333f2def0925d3
Patrik Kozak <patrik@trbl.design>
2023-01-24 13:39:42 -0500

    chore: manually adds auto-scroll to hash linked urls

---
+++ src/providers/PageTransition/index.tsx
@@ -0,0 +1,48 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+
+export const pageTransTime = 400;
+
+export const PageTransition: React.FC<{
+  children: React.ReactNode
+}> = (props) => {
+  const { children } = props;
+  const hasInitialized = useRef(false); // don't scroll to top on first render
+  const nodeRef = useRef(null);
+
+  const handleTransition = useCallback(() => {
+    document.documentElement.style.scrollBehavior = 'auto'; // instantly scroll
+
+    const scrollToTopTimer = setTimeout(() => {
+      window.scrollTo(0, 0);
+      document.documentElement.style.removeProperty('scroll-behavior');
+    }, pageTransTime);
+
+    const { hash } = window?.location;
+    let scrollToHashTimer: NodeJS.Timeout;
+
+    if (hash) {
+      scrollToHashTimer = setTimeout(() => {
+        const hashWithoutMark = hash.substring(1);
+        const element = document.getElementById(hashWithoutMark);
+        element?.scrollIntoView();
+      }, pageTransTime);
+
+      return () => {
+        if (scrollToTopTimer) clearTimeout(scrollToTopTimer)
+        if (scrollToHashTimer) clearTimeout(scrollToHashTimer)
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (hasInitialized.current) {
+      handleTransition(); // on every route change
+    } else hasInitialized.current = true;
+  }, [handleTransition]);
+
+  return (
+    <div ref={nodeRef}>
+      {children}
+    </div>
+  )
+}
