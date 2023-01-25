1e09b80a02210f613a45aab5853a469689888c46
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-01-25 13:11:14 -0500

    chore: adds try catch to auth requests

--- src/providers/Auth/index.tsx
+++ src/providers/Auth/index.tsx
@@ -18,56 +18,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const [user, setUser] = useState<User | null>()
 
   const login = useCallback<Login>(async args => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_CLOUD_CMS_URL}/api/graphql`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: `mutation {
-            loginUser(email: "${args.email}", password: "${args.password}") {
-              user {
-                email
-              }
-              exp
-            }
-          }`,
-      }),
-    })
-
-    if (res.ok) {
-      const { data, errors } = await res.json()
-      if (errors) throw new Error(errors[0].message)
-      setUser(data?.loginUser?.user)
-    } else {
-      throw new Error('Invalid login')
-    }
-  }, [])
-
-  const logout = useCallback<Logout>(async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_CLOUD_CMS_URL}/api/graphql`, {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: `mutation {
-            logoutUser
-          }`,
-      }),
-    })
-
-    if (res.ok) {
-      setUser(null)
-    } else {
-      throw new Error('There was a problem while logging out.')
-    }
-  }, [])
-
-  useEffect(() => {
-    const fetchMe = async () => {
+    try {
       const res = await fetch(`${process.env.NEXT_PUBLIC_CLOUD_CMS_URL}/api/graphql`, {
         method: 'POST',
         credentials: 'include',
@@ -75,22 +26,84 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          query: `query {
-            meUser {
-              user {
-                email
+          query: `mutation {
+              loginUser(email: "${args.email}", password: "${args.password}") {
+                user {
+                  email
+                }
+                exp
               }
-              exp
-            }
-          }`,
+            }`,
         }),
       })
 
       if (res.ok) {
-        const { data } = await res.json()
-        setUser(data?.meUser?.user || null)
+        const { data, errors } = await res.json()
+        if (errors) throw new Error(errors[0].message)
+        setUser(data?.loginUser?.user)
       } else {
-        throw new Error('There was a problem while logging out.')
+        throw new Error('Invalid login')
+      }
+    } catch (e) {
+      throw new Error('An error occurred while attempting to login.')
+    }
+  }, [])
+
+  const logout = useCallback<Logout>(async () => {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_CLOUD_CMS_URL}/api/graphql`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `mutation {
+              logoutUser
+            }`,
+        }),
+      })
+
+      if (res.ok) {
+        setUser(null)
+      } else {
+        throw new Error('An error occurred while attempting to logout.')
+      }
+    } catch (e) {
+      throw new Error('An error occurred while attempting to logout.')
+    }
+  }, [])
+
+  useEffect(() => {
+    const fetchMe = async () => {
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_CLOUD_CMS_URL}/api/graphql`, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            query: `query {
+              meUser {
+                user {
+                  email
+                }
+                exp
+              }
+            }`,
+          }),
+        })
+
+        if (res.ok) {
+          const { data } = await res.json()
+          setUser(data?.meUser?.user || null)
+        } else {
+          throw new Error('An error occurred while fetching your account.')
+        }
+      } catch (e) {
+        setUser(null)
+        throw new Error('An error occurred while fetching your account.')
       }
     }
 
