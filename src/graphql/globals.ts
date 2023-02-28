bd7c668c1af06cf965b119edb73e6d8212317fe3
Jessica Boezwinkle <jessica@trbl.design>
2023-02-28 17:42:06 +0000

    fix: removes announcement from global query

--- src/graphql/globals.ts
+++ src/graphql/globals.ts
@@ -2,10 +2,6 @@ import { LINK_FIELDS } from './link'
 
 export const GLOBALS = `
   query {
-    Announcement {
-      message
-    }
-
     MainMenu {
       navItems {
         link ${LINK_FIELDS({ disableAppearance: true })}
