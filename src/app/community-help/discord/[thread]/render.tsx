181578d0b3e21de6c121f3845fa8387de36e3489
Patrik Kozak <patrik@trbl.design>
2023-03-07 22:25:42 -0500

    fix: updates prettier

--- src/app/community-help/discord/[thread]/render.tsx
+++ src/app/community-help/discord/[thread]/render.tsx
@@ -19,13 +19,7 @@ export type Attachments = {
   proxyURL: string
   height: number
   width: number
-  contentType:
-  | 'image/png'
-  | 'video/MP2T'
-  | 'text/plain'
-  | 'application/json'
-  | 'video/quicktime'
-  | 'image/jpeg'
+  contentType: 'image/png' | 'video/MP2T' | 'text/plain' | 'application/json' | 'video/quicktime' | 'image/jpeg'
   description: string
   ephemeral: boolean
 }[]
