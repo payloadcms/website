2d75e140a83791ac212f9a49a2a0e335b6da46e0
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-03-16 15:12:14 -0400

    chore: adjusts sitemap env

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://payloadcms.com',
+  siteUrl: process.env.SITEMAP_URL || 'https://payloadcms.com',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
