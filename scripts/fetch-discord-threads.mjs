7890f0bfc913dbd40cc59401776e164058734776
Jessica Boezwinkle <jessica@trbl.design>
2023-03-16 15:03:54 +0000

    fix: matches slugify function across scripts

--- scripts/fetch-discord-threads.mjs
+++ scripts/fetch-discord-threads.mjs
@@ -7,7 +7,23 @@ import dotenv from 'dotenv'
 import { Bar } from 'cli-progress'
 import DiscordMarkdown from 'discord-markdown'
 import { fileURLToPath } from 'url'
-import slugify from '../src/utilities/slugify'
+
+function slugify(string) {
+  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
+  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
+  const p = new RegExp(a.split('').join('|'), 'g')
+
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+    .replace(/\-\-+/g, '-') // Replace multiple - with single -
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, '') // Trim - from end of text
+}
 
 const { toHTML } = DiscordMarkdown
 
