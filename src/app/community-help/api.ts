74e0b1f68b78bf00cc00c97117aa63ff06002594
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-03-16 09:56:41 -0400

    chore: declares types coming back from json files without 'any'

--- src/app/community-help/api.ts
+++ src/app/community-help/api.ts
@@ -3,26 +3,24 @@ import threads from '../../../threads.json'
 import type { ThreadProps } from './discord/[thread]/render'
 import type { DiscussionProps } from './github/[discussion]/render'
 
-// TODO: Update types
-
 export async function getAllDiscussions(): Promise<DiscussionProps[] | undefined> {
   return discussions.map(discussion => ({ ...discussion })) as any as DiscussionProps[] // TODO: Fix this
 }
 
 export async function getDiscussion(slug: string): Promise<DiscussionProps | undefined> {
-  const matchedDiscussion = discussions.find(
-    discussion => discussion.slug === slug,
-  ) as any as DiscussionProps // TODO: Fix this
+  const matchedDiscussion = discussions.find(discussion => discussion.slug === slug) as
+    | DiscussionProps
+    | undefined
 
   return matchedDiscussion
 }
 
 export async function getAllThreads(): Promise<ThreadProps[] | undefined> {
-  return threads.map(thread => ({ ...thread })) as any as ThreadProps[] // TODO: Fix this
+  return threads.map(thread => ({ ...thread })) as ThreadProps[]
 }
 
 export async function getThread(slug: string): Promise<ThreadProps | undefined> {
-  const matchedThread = threads.find(thread => thread.slug === slug) as any as ThreadProps // TODO: Fix this
+  const matchedThread = threads.find(thread => thread.slug === slug) as ThreadProps
 
   return matchedThread
 }
