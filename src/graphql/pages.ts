8c81862b1f5fbb19f611818d6a58e13fd0f16822
Jacob Fletcher <jacobsfletch@gmail.com>
2023-03-27 13:24:39 -0400

    fix: build errors

--- src/graphql/pages.ts
+++ src/graphql/pages.ts
@@ -23,7 +23,7 @@ import { META_FIELDS } from './meta'
 
 export const PAGES = `
   query Pages {
-    Pages(limit: 300, where: { slug: { notEquals: "cloud" } }) {
+    Pages(limit: 300, where: { slug: { not_equals: "cloud" } }) {
       docs {
         slug
         breadcrumbs {
