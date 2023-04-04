0705b288db8c241468e4fc23e5942376db6187f3
Jessica Boezwinkle <jessica@trbl.design>
2023-04-04 15:41:40 +0100

    chore: remove next link underline for CH

--- src/app/community-help/render.tsx
+++ src/app/community-help/render.tsx
@@ -43,6 +43,7 @@ export const CommunityHelp: React.FC = () => {
                       <Link
                         className={classes.postContent}
                         href={`/community-help/${platform.toLowerCase()}/${slug}`}
+                        style={{ textDecoration: 'none' }}
                       >
                         <div>
                           <h5 className={classes.title}>{name}</h5>
