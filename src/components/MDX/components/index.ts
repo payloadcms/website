dae1b55ddc5379be256e5781b35e9458b6b28089
Max Morozov <max@morozov.page>
2023-04-07 18:23:31 +0200

    fix: eslint import sort

--- src/components/MDX/components/index.ts
+++ src/components/MDX/components/index.ts
@@ -2,11 +2,11 @@ import { Banner } from '../../Banner'
 import YouTube from '../../YouTube'
 import BlogImage from './BlogImage'
 import Code from './Code'
-import InlineCode from './InlineCode'
 import h1 from './H1'
 import h2 from './H2'
 import h3 from './H3'
 import HR from './HR'
+import InlineCode from './InlineCode'
 import table from './Table'
 
 export default {
