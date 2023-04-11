ad02e53b1edc9bcc36aa977fc6f9d96437fd8662
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-04-10 21:56:08 -0400

    fix: merging of og images

--- src/seo/mergeOpenGraph.ts
+++ src/seo/mergeOpenGraph.ts
@@ -17,5 +17,6 @@ export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'
   return {
     ...defaultOpenGraph,
     ...og,
+    images: og?.images ? og.images : defaultOpenGraph.images,
   }
 }
