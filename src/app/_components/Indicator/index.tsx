c3d8a0ef4079c1fb8752bc5351d0eed97c432e12
Jacob Fletcher <jacobsfletch@gmail.com>
2023-04-13 11:19:37 -0400

    chore: extracts spinner from indicator for reuse

--- src/app/_components/Indicator/index.tsx
+++ src/app/_components/Indicator/index.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react'
 
+import { Spinner } from '../Spinner'
+
 import classes from './index.module.scss'
 
 type IndicatorProps = {
   status: 'success' | 'error' | 'warning' | 'info'
   spinner?: boolean
 }
+
 export const Indicator: React.FC<IndicatorProps> = ({ status = 'success', spinner = false }) => {
   return (
-    <div
-      className={[classes.indicator, classes[`status--${status}`], spinner && classes.spinner]
-        .filter(Boolean)
-        .join(' ')}
-    />
+    <div className={[classes.indicator, classes[`status--${status}`]].filter(Boolean).join(' ')}>
+      {spinner && <Spinner />}
+    </div>
   )
 }
