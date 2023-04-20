e89b9433aff4247ec2cc823f3e87e6c6ea1b92b5
Jessica Boezwinkle <jessica@trbl.design>
2023-04-20 17:28:46 +0100

    feat: adds delete account button to cloud settings page

--- src/app/cloud/settings/client_page.tsx
+++ src/app/cloud/settings/client_page.tsx
@@ -1,26 +1,77 @@
 'use client'
 
 import React, { Fragment, useCallback } from 'react'
+import { toast } from 'react-toastify'
 import { Cell, Grid } from '@faceless-ui/css-grid'
+import { useModal } from '@faceless-ui/modal'
 import { Text } from '@forms/fields/Text'
 import Form from '@forms/Form'
 import Submit from '@forms/Submit'
 import { OnSubmit } from '@forms/types'
+import { useRouter } from 'next/navigation'
 
 import { Button } from '@components/Button'
 import { Gutter } from '@components/Gutter'
 import { Heading } from '@components/Heading'
 import { Message } from '@components/Message'
+import { ModalWindow } from '@components/ModalWindow'
 import { useAuth } from '@root/providers/Auth'
 
 import classes from './page.module.scss'
 
+const modalSlug = 'delete-account'
+
+const ModalContent = () => {
+  const { closeModal } = useModal()
+  const [hasEmail, setHasEmail] = React.useState(false)
+  const [hasPW, setHasPW] = React.useState(false)
+
+  return (
+    <>
+      <Heading marginTop={false} as="h5">
+        Are you sure you want to delete your account?
+      </Heading>
+      <Message className={classes.warning} error={'Deleting your account cannot be undone.'} />
+      <p>
+        Team ownership will be transferred to another team member where possible. If no other team
+        members exist, the team and associated projects / deployments will be
+        <strong> permanently deleted</strong>.
+      </p>
+      <p>To proceed re-enter your account details below:</p>
+      <Text
+        className={classes.emailInput}
+        label="Email"
+        path="modalEmail"
+        required
+        onChange={value => {
+          setHasEmail(Boolean(value))
+        }}
+      />
+      <Text
+        label="Password"
+        path="modalPassword"
+        type="password"
+        required
+        onChange={value => {
+          setHasPW(Boolean(value))
+        }}
+      />
+      <div className={classes.modalActions}>
+        <Button label="cancel" appearance="secondary" onClick={() => closeModal(modalSlug)} />
+        <Submit label="delete my account" appearance="danger" disabled={!(hasEmail && hasPW)} />
+      </div>
+    </>
+  )
+}
+
 export const SettingsPage = () => {
-  const { user, updateUser } = useAuth()
+  const { user, updateUser, login } = useAuth()
+  const { openModal } = useModal()
   const [loading, setLoading] = React.useState<boolean>(false)
   const [formToShow, setFormToShow] = React.useState<'account' | 'password'>('account')
   const [error, setError] = React.useState<string | null>(null)
   const [success, setSuccess] = React.useState<string | null>(null)
+  const router = useRouter()
 
   const handleSubmit: OnSubmit = useCallback(
     async ({ data, dispatchFields }): Promise<void> => {
@@ -77,6 +128,38 @@ export const SettingsPage = () => {
     },
     [updateUser],
   )
+
+  const deleteAccount = React.useCallback(async ({ data }) => {
+    if (user) {
+      try {
+        const confirmedUser = await login({
+          email: data.modalEmail as string,
+          password: data.modalPassword as string,
+        })
+
+        if (confirmedUser && confirmedUser.id === user.id) {
+          try {
+            const req = await fetch(
+              `${process.env.NEXT_PUBLIC_CLOUD_CMS_URL}/api/users/${user.id}`,
+              {
+                method: 'DELETE',
+                credentials: 'include',
+              },
+            )
+
+            if (req.status === 200) {
+              toast.success('Your account was successfully deleted.')
+              router.push('/logout')
+            }
+          } catch (e) {
+            toast.error('There was an issue deleting your account. Please try again.')
+          }
+        }
+      } catch (e) {
+        toast.error('Incorrect email or password.')
+      }
+    }
+  }, [])
 
   return (
     <Gutter className={classes.content}>
@@ -181,7 +264,22 @@ export const SettingsPage = () => {
         </Cell>
       </Grid>
       <hr className={classes.hr} />
-      <Button label="Log out" appearance="secondary" href="/logout" el="link" />
+      <div className={classes.buttonWrap}>
+        <Button label="Log out" appearance="secondary" href="/logout" el="link" />
+        <Button
+          className={classes.deleteAccount}
+          label="Delete account"
+          appearance="danger"
+          onClick={() => {
+            openModal(modalSlug)
+          }}
+        />
+      </div>
+      <ModalWindow className={classes.modal} slug={modalSlug}>
+        <Form onSubmit={deleteAccount}>
+          <ModalContent />
+        </Form>
+      </ModalWindow>
     </Gutter>
   )
 }
