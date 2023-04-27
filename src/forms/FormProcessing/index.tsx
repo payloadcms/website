cfdeaebb272b6ce277cd389f556ab9a01cabaf04
Jacob Fletcher <jacobsfletch@gmail.com>
2023-04-27 16:34:59 -0400

    feat: optionally debounces FormProcessing

--- src/forms/FormProcessing/index.tsx
+++ src/forms/FormProcessing/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { useFormProcessing } from '@forms/Form/context'
 
+import useDebounce from '@root/utilities/use-debounce'
+
 const FormProcessing: React.FC<{
   className?: string
   message?: string
+  delay?: number
 }> = props => {
   const { className, message = 'Processing...' } = props
 
   const isProcessing = useFormProcessing()
+  const debouncedIsProcessing = useDebounce(isProcessing, props.delay || 0)
 
-  if (isProcessing) {
+  if (debouncedIsProcessing) {
     return <p className={[className].filter(Boolean).join(' ')}>{message}</p>
   }
 
