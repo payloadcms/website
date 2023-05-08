ee26e8495ae2b85cb778234cfdbd81e8cd81d3b0
Jarrod Flesch <jarrodmflesch@gmail.com>
2023-05-08 16:23:11 -0400

    chore: add dns prefetch tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,10 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="en">
       <head>
         <link rel="icon" href="/images/favicon.svg" />
+        <link rel="dns-prefetch" href={process.env.NEXT_PUBLIC_CLOUD_CMS_URL} />
+        <link rel="dns-prefetch" href="https://api.github.com/repos/payloadcms/payload" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3" />
         <GoogleAnalytics />
-        <link rel="dns-prefetch" href={process.env.NEXT_PUBLIC_CLOUD_CMS_URL} />
       </head>
       <body
         className={[
