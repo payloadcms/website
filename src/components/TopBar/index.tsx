928d2fec9859d7a6703c421484a102fe87f4656a
Patrik Kozak <patrik@trbl.design>
2023-05-12 10:38:16 -0400

    fix: shows top bar when mobile menu open

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,13 +1,17 @@
 'use client'
 
 import * as React from 'react'
+import { useModal } from '@faceless-ui/modal'
 
+import { modalSlug } from '@components/Header/MobileNav'
 import { RichText } from '@components/RichText'
 import type { TopBar as TopBarType } from '../../payload-types'
 
 import classes from './index.module.scss'
 
 export const TopBar: React.FC<TopBarType> = ({ starText, announcement }) => {
+  const { isModalOpen } = useModal()
+  const isMobileNavOpen = isModalOpen(modalSlug)
   const [starCount, setStarCount] = React.useState<number | undefined>()
 
   React.useEffect(() => {
@@ -22,7 +26,11 @@ export const TopBar: React.FC<TopBarType> = ({ starText, announcement }) => {
   }, [])
 
   return (
-    <div className={classes.topBar}>
+    <div
+      className={[classes.topBar, isMobileNavOpen && classes.mobileNavOpen]
+        .filter(Boolean)
+        .join(' ')}
+    >
       {typeof starCount === 'number' && (
         <div className={classes.wrap}>
           <div className={classes.starWrap}>
