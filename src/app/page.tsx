0bd24f1a22335e5c56a1e4fdf789e14b7c2fe260
James <james@trbl.design>
2023-05-30 20:50:19 -0400

    chore: further comments out metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import PageTemplate, { generateMetadata } from './(pages)/[...slug]/page'
+import PageTemplate from './(pages)/[...slug]/page'
 
 export default PageTemplate
 
-export { generateMetadata }
+// export { generateMetadata }
