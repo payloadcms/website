c57e7385a742aae7c75fae985e47a39a1b89e2b2
Elliot DeNolf <denolfe@gmail.com>
2023-06-22 16:25:42 -0400

    feat: add current build indicator, sha, message

---
+++ src/graphics/ClockIcon/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const ClockIcon: React.FC<{
+  className?: string
+}> = props => {
+  const { className } = props
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className={className}
+    >
+      <circle cx="12" cy="12" r="10" />
+      <polyline points="12 6 12 12 16.5 12" />
+    </svg>
+  )
+}
