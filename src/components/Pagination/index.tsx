492a8352251c1c2f8c85a0cb2428692f5783a794
Jessica Boezwinkle <jessica@trbl.design>
2023-06-30 11:19:57 +0100

    chore: update import project pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 
+import { ChevronIconV2 } from '@root/graphics/ChevronIconV2'
+
 import classes from './index.module.scss'
 
 export const Pagination: React.FC<{
@@ -7,36 +9,112 @@ export const Pagination: React.FC<{
   setPage: (page: number) => void
   totalPages: number
 }> = ({ page, setPage, totalPages }) => {
+  const [indexToShow, setIndexToShow] = React.useState([0, 1, 2, 3, 4])
+  const showFirstPage = totalPages > 5 && page >= 2
+  const showLastPage = totalPages > 5 && page <= totalPages - 3
+
+  React.useEffect(() => {
+    if (showFirstPage && showLastPage) {
+      setIndexToShow([1, 2, 3])
+    }
+
+    if (showFirstPage && !showLastPage) {
+      setIndexToShow([2, 3, 4])
+    }
+
+    if (!showFirstPage && showLastPage) {
+      setIndexToShow([0, 1, 2])
+    }
+
+    if (!showFirstPage && !showLastPage) {
+      setIndexToShow([0, 1, 2, 3, 4])
+    }
+  }, [showFirstPage, showLastPage])
+
   return (
     <div className={classes.pagination}>
+      {showFirstPage && (
+        <>
+          <button
+            type="button"
+            className={classes.paginationButton}
+            onClick={() => {
+              window.scrollTo(0, 0)
+              setPage(1)
+            }}
+          >
+            1
+          </button>
+          <div className={classes.dash}>&mdash;</div>
+        </>
+      )}
+      {[...Array(totalPages)].map((_, index) => {
+        const currentPage = index + 1
+        const isCurrent = page === currentPage
+
+        if (indexToShow.includes(index))
+          return (
+            <div key={index}>
+              <button
+                type="button"
+                className={[
+                  classes.paginationButton,
+                  isCurrent && classes.paginationButtonActive,
+                  isCurrent && classes.disabled,
+                ]
+                  .filter(Boolean)
+                  .join(' ')}
+                onClick={() => {
+                  window.scrollTo(0, 0)
+                  setPage(currentPage)
+                }}
+              >
+                {currentPage}
+              </button>
+            </div>
+          )
+      })}
+      {showLastPage && (
+        <>
+          <div className={classes.dash}>&mdash;</div>
+          <button
+            type="button"
+            className={classes.paginationButton}
+            onClick={() => {
+              setTimeout(() => {
+                window.scrollTo(0, 0)
+              }, 0)
+              setPage(totalPages)
+            }}
+          >
+            {totalPages}
+          </button>
+        </>
+      )}
       <button
         disabled={page - 1 < 1}
         onClick={() => {
           if (page - 1 < 1) return
-
-          setTimeout(() => {
-            window.scrollTo(0, 0)
-          }, 0)
+          window.scrollTo(0, 0)
           setPage(page > 1 ? page - 1 : 1)
         }}
-        className={classes.button}
+        className={[classes.button, page - 1 < 1 && classes.disabled].filter(Boolean).join(' ')}
       >
-        &#8249;
+        <ChevronIconV2 rotation={180} />
       </button>
-      <span className={classes.page}>{`Page ${page} of ${totalPages}`}</span>
       <button
         disabled={page + 1 > totalPages}
         onClick={() => {
           if (page + 1 > totalPages) return
 
-          setTimeout(() => {
-            window.scrollTo(0, 0)
-          }, 0)
+          window.scrollTo(0, 0)
           setPage(page + 1)
         }}
-        className={classes.button}
+        className={[classes.button, page + 1 > totalPages && classes.disabled]
+          .filter(Boolean)
+          .join(' ')}
       >
-        &#8250;
+        <ChevronIconV2 />
       </button>
     </div>
   )
