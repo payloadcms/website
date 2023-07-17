6e85ce8a1e92c181071c466626dd1440069ea785
Jessica Boezwinkle <jessica@trbl.design>
2023-07-17 15:55:07 +0100

    chore: updates light/dark image component for docs

--- src/components/MDX/components/LightDarkImage/index.tsx
+++ src/components/MDX/components/LightDarkImage/index.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
+import { useThemePreference } from '@root/providers/Theme'
+
 import classes from './index.module.scss'
 
 const LightDarkImage: React.FC<{
@@ -8,12 +10,16 @@ const LightDarkImage: React.FC<{
   srcDark: string
   alt: string
   caption?: string
-}> = ({ srcLight, srcDark, alt, caption }) => (
-  <div className={classes.imageWrap}>
-    <img className={classes.imgLight} src={srcLight} alt={alt} />
-    <img className={classes.imgDark} src={srcDark} alt={alt} />
-    {caption && <div className={classes.caption}>{caption}</div>}
-  </div>
-)
+}> = ({ srcLight, srcDark, alt, caption }) => {
+  const { theme } = useThemePreference()
+  const src = theme === 'dark' ? srcDark : srcLight
+
+  return (
+    <div className={classes.imageWrap}>
+      <img src={src} alt={alt} />
+      {caption && <div className={classes.caption}>{caption}</div>}
+    </div>
+  )
+}
 
 export default LightDarkImage
