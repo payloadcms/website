ee1dc06abc40c858f1aa177de756d8704c22aa1e
Jacob Fletcher <jacobsfletch@gmail.com>
2023-08-01 13:11:59 -0400

    fix: incorrect svg props

--- src/graphics/CommitIcon/index.tsx
+++ src/graphics/CommitIcon/index.tsx
@@ -13,9 +13,9 @@ export const CommitIcon: React.FC<Props> = ({ className }) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <circle cx="12" cy="12" r="4"></circle>
       <line x1="1.05" y1="12" x2="7" y2="12"></line>
