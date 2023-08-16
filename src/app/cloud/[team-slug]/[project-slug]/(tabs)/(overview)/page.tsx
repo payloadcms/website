0de52f545bd524560c6470e5b2fb8324ee963363
Jacob Fletcher <jacobsfletch@gmail.com>
2023-08-16 09:22:26 -0400

    chore: re-adds badly cased files

--- src/app/cloud/[team-slug]/[project-slug]/(tabs)/(overview)/page.tsx
+++ src/app/cloud/[team-slug]/[project-slug]/(tabs)/(overview)/page.tsx
@@ -1,10 +1,17 @@
 import { fetchProjectAndRedirect } from '@cloud/_api/fetchProject'
 import { Metadata } from 'next'
 
+import { InfraOffline } from './InfraOffline'
+import { InfraOnline } from './InfraOnline'
+
 export default async ({ params: { 'team-slug': teamSlug, 'project-slug': projectSlug } }) => {
   const { team, project } = await fetchProjectAndRedirect({ teamSlug, projectSlug })
 
-  return null
+  if (project?.infraStatus === 'done') {
+    return <InfraOnline project={project} />
+  }
+
+  return <InfraOffline project={project} team={team} />
 }
 
 export const metadata: Metadata = {
