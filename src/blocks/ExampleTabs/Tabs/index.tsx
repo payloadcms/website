671d2baafbfdd80222f7372d4c9235fac3d35833
Tylan Davis <hello@tylandavis.com>
2023-09-22 16:13:39 -0400

    fix overflow on tab navigation

--- src/blocks/ExampleTabs/Tabs/index.tsx
+++ src/blocks/ExampleTabs/Tabs/index.tsx
@@ -16,18 +16,20 @@ export const Tabs: React.FC<Props> = props => {
 
   return (
     <div className={classes.tabsContainer}>
-      <nav className={classes.tabNavigation}>
-        {tabs &&
-          tabs.map((tab, i) => (
-            <button
-              key={i}
-              onClick={() => setActiveTab(i)}
-              className={i === activeTab ? classes.active : ''}
-            >
-              {tab.label}
-            </button>
-          ))}
-      </nav>
+      <div className={classes.navigationWrap}>
+        <nav className={classes.tabNavigation}>
+          {tabs &&
+            tabs.map((tab, i) => (
+              <button
+                key={i}
+                onClick={() => setActiveTab(i)}
+                className={i === activeTab ? classes.active : ''}
+              >
+                {tab.label}
+              </button>
+            ))}
+        </nav>
+      </div>
       {tabs && <RichText content={tabs[activeTab].content} className={classes.content} />}
       {tabs && <Examples examples={tabs[activeTab].examples} />}
     </div>
