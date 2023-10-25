b22183bf4932b7b00903485c82afbec77ee4d684
Patrik Kozak <patrik@trbl.design>
2023-10-25 12:56:22 -0400

    chore: removes error throwing if no token, instead don't send token

--- src/app/cloud/_api/fetchTemplates.ts
+++ src/app/cloud/_api/fetchTemplates.ts
@@ -5,7 +5,6 @@ import { payloadCloudToken } from './token'
 export const fetchTemplates = async (): Promise<Template[]> => {
   const { cookies } = await import('next/headers')
   const token = cookies().get(payloadCloudToken)?.value ?? null
-  if (!token) throw new Error('No token provided')
 
   const doc: Template[] = await fetch(`${process.env.NEXT_PUBLIC_CLOUD_CMS_URL}/api/graphql`, {
     method: 'POST',
