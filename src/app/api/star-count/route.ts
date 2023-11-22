e85696103410f1f3a37c8c1e602872a4736d7210
Elliot DeNolf <denolfe@gmail.com>
2023-11-22 10:25:14 -0500

    feat: move star count to route

---
+++ src/app/api/star-count/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server'
+
+export const revalidate = 900
+
+export async function GET(): Promise<NextResponse> {
+  const { stargazers_count: totalStars } = await fetch(
+    'https://api.github.com/repos/payloadcms/payload',
+  ).then(res => res.json())
+
+  return NextResponse.json({ totalStars })
+}
