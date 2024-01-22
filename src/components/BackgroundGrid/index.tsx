f808ab19bd4e6fb218dcb298031117f2b720f04c
Paul Popus <paul@nouance.io>
2024-01-22 15:40:54 -0300

    Update backgroundgrid to disable gutter

--- src/components/BackgroundGrid/index.tsx
+++ src/components/BackgroundGrid/index.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 import classes from './index.module.scss'
 
-export const BackgroundGrid: React.FC = () => {
+interface Props {
+  className?: string
+  ignoreGutter?: boolean
+}
+export const BackgroundGrid: React.FC<Props> = ({ className, ignoreGutter }: Props) => {
   return (
-    <div className={[classes.backgroundGrid, 'grid'].filter(Boolean).join(' ')}>
+    <div
+      aria-hidden={true}
+      className={[classes.backgroundGrid, 'grid', ignoreGutter && classes.ignoreGutter, className]
+        .filter(Boolean)
+        .join(' ')}
+    >
       <div className={[classes.column, 'cols-4'].filter(Boolean).join(' ')}></div>
       <div className={[classes.column, 'cols-4'].filter(Boolean).join(' ')}></div>
       <div className={[classes.column, 'cols-4'].filter(Boolean).join(' ')}></div>
