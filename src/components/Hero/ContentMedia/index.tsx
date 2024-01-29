18dc1b67e922905f546ca3c2130aecdbbc0385f6
Paul Popus <paul@nouance.io>
2024-01-29 16:19:57 -0300

    remove styling differences betwene wide and normal

--- src/components/Hero/ContentMedia/index.tsx
+++ src/components/Hero/ContentMedia/index.tsx
@@ -13,43 +13,21 @@ import { Page } from '@root/payload-types'
 import classes from './index.module.scss'
 
 export const ContentMediaHero: React.FC<
-  Pick<Page['hero'], 'richText' | 'media' | 'mediaWidth' | 'links' | 'sidebarContent'> & {
+  Pick<Page['hero'], 'richText' | 'media' | 'links' | 'sidebarContent'> & {
     breadcrumbs?: Page['breadcrumbs']
   }
-> = ({ richText, media, mediaWidth, links, sidebarContent }) => {
+> = ({ richText, media, links, sidebarContent }) => {
   return (
     <Gutter>
-      <div
-        className={[
-          classes.wrapper,
-          mediaWidth === 'wide' ? classes.wideGrid : classes.normalGrid,
-          'grid',
-        ]
-          .filter(Boolean)
-          .join(' ')}
-      >
+      <div className={[classes.wrapper, 'grid'].filter(Boolean).join(' ')}>
         <BackgroundGrid ignoreGutter />
-        <div
-          className={[
-            `cols-${mediaWidth === 'wide' ? 4 : 7}`,
-            `cols-l-${mediaWidth === 'wide' ? 4 : 7}`,
-            'cols-m-8 start-1',
-            'grid',
-          ]
-            .filter(Boolean)
-            .join(' ')}
-        >
-          <RichText
-            content={richText}
-            className={[classes.richText, 'cols-8 start-1'].filter(Boolean).join(' ')}
-          />
+        <div className={[classes.sidebar, `cols-4`, 'cols-m-8 start-1'].filter(Boolean).join(' ')}>
+          <RichText content={richText} className={[classes.richText].filter(Boolean).join(' ')} />
 
-          <div
-            className={[classes.linksWrapper, 'cols-16 cols-m-8 start-1'].filter(Boolean).join(' ')}
-          >
+          <div className={[classes.linksWrapper].filter(Boolean).join(' ')}>
             <RichText
               content={sidebarContent}
-              className={[classes.sidebarContent, 'cols-8 start-1'].filter(Boolean).join(' ')}
+              className={[classes.sidebarContent].filter(Boolean).join(' ')}
             />
 
             {Array.isArray(links) &&
@@ -69,22 +47,14 @@ export const ContentMediaHero: React.FC<
         </div>
         {typeof media === 'object' && media !== null && (
           <div
-            className={[
-              `start-${mediaWidth === 'wide' ? 7 : 10}`,
-              `start-l-${mediaWidth === 'wide' ? 7 : 10}`,
-              `cols-${mediaWidth === 'wide' ? 10 : 7}`,
-              `cols-l-${mediaWidth === 'wide' ? 10 : 7}`,
-              'cols-m-8 start-m-1',
-            ]
+            className={[`start-7`, `start-l-7`, `cols-10`, `cols-l-10`, 'cols-m-8 start-m-1']
               .filter(Boolean)
               .join(' ')}
           >
-            <div className={mediaWidth === 'wide' ? classes.wideMedia : classes.media}>
+            <div className={classes.media}>
               <Media
                 resource={media}
-                sizes={`100vw, (max-width: 1920px) ${
-                  mediaWidth === 'wide' ? '50vw' : '75vw'
-                }, (max-width: 1024px) 100vw`}
+                sizes={`100vw, (max-width: 1920px) 75vw, (max-width: 1024px) 100vw`}
               />
             </div>
           </div>
