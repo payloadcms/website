15d548524c6c8e9ac11f20e9ac83aa9bb961f6ef
Paul Popus <paul@nouance.io>
2024-02-05 11:40:43 -0300

    Update callout stylings

--- src/blocks/Callout/index.tsx
+++ src/blocks/Callout/index.tsx
@@ -14,21 +14,27 @@ import classes from './index.module.scss'
 
 export type CalloutProps = Extract<Page['layout'][0], { blockType: 'callout' }>
 
-const QuoteStyle: React.FC<Pick<CalloutProps['calloutFields'], 'quote' | 'author' | 'logo'>> = ({
+const QuoteContent: React.FC<Pick<CalloutProps['calloutFields'], 'quote' | 'author' | 'logo'>> = ({
   quote,
   author,
   logo,
 }) => {
   return (
-    <div className={[classes.quoteStyle, 'cols-6 start-2 cols-m-8'].filter(Boolean).join(' ')}>
-      <QuoteIconAlt className={classes.quoteIcon} />
-      <div className={[classes.quote, 'cols-12'].filter(Boolean).join(' ')}>{quote}”</div>
+    <div
+      className={[classes.quoteContent, 'cols-6 start-2 cols-m-8 start-m-1']
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <div className={classes.quoteContainer}>
+        <QuoteIconAlt className={classes.quoteIcon} />
+        <div className={[classes.quote, 'cols-12'].filter(Boolean).join(' ')}>{quote}”</div>
 
-      <div className={[classes.authorWrapper, 'cols-12'].filter(Boolean).join(' ')}>
-        <div className={classes.logo}>
-          {logo && typeof logo !== 'string' && <Media resource={logo} />}
+        <div className={[classes.authorWrapper, 'cols-12'].filter(Boolean).join(' ')}>
+          <div className={classes.logo}>
+            {logo && typeof logo !== 'string' && <Media resource={logo} />}
+          </div>
+          <div className={classes.author}>{author}</div>
         </div>
-        <div className={classes.author}>{author}</div>
       </div>
     </div>
   )
@@ -36,27 +42,32 @@ const QuoteStyle: React.FC<Pick<CalloutProps['calloutFields'], 'quote' | 'author
 
 export const Callout: React.FC<CalloutProps> = props => {
   const {
-    calloutFields: { richText, links, quote, style, author, logo, media },
+    calloutFields: { richText, links, quote, style: calloutStyle, author, logo, media },
   } = props
 
   const hasLinks = links && links.length > 0
+  const style = calloutStyle ?? 'default'
+  const styleClass = style === 'default' ? classes.defaultStyle : classes.quoteStyle
 
   return (
     <BlockSpacing>
       <div className={classes.wrapper}>
         <Gutter>
           <BackgroundGrid className={classes.backgroundGrid} />
-          <div className={[classes.container, 'grid'].filter(Boolean).join(' ')}>
+          <div className={[classes.container, 'grid', styleClass].filter(Boolean).join(' ')}>
             {style === 'quote' ? (
-              <QuoteStyle quote={quote} author={author} logo={logo} />
+              <QuoteContent quote={quote} author={author} logo={logo} />
             ) : (
               <div
-                className={[classes.defaultStyle, 'cols-6 start-2 cols-m-8']
+                className={[classes.contentWrapper, 'cols-6 start-2 cols-m-8 start-m-1']
                   .filter(Boolean)
                   .join(' ')}
               >
-                <RichText content={richText} className={classes.content} />
-                <div className={[classes.linksContainer, ''].filter(Boolean).join(' ')}>
+                <RichText
+                  content={richText}
+                  className={[classes.content].filter(Boolean).join(' ')}
+                />
+                <div className={[classes.linksContainer].filter(Boolean).join(' ')}>
                   {hasLinks && (
                     <div className={[classes.links, ''].filter(Boolean).join(' ')}>
                       {links.map(({ link }, index) => (
@@ -66,6 +77,7 @@ export const Callout: React.FC<CalloutProps> = props => {
                           appearance={'default'}
                           buttonProps={{
                             appearance: 'default',
+                            hideBottomBorderExceptLast: true,
                           }}
                           className={[classes.link].filter(Boolean).join(' ')}
                         />
@@ -75,7 +87,11 @@ export const Callout: React.FC<CalloutProps> = props => {
                 </div>
               </div>
             )}
-            <div className={[classes.media, 'cols-8 start-9'].filter(Boolean).join(' ')}>
+            <div
+              className={[classes.media, styleClass, 'cols-8 start-9 cols-m-8 start-m-1']
+                .filter(Boolean)
+                .join(' ')}
+            >
               {media && typeof media !== 'string' && <Media resource={media} />}
             </div>
           </div>
