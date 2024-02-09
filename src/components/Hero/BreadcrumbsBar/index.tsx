00227b34a97be3e96ebc38fd6df081c9f793f4f9
Paul <paul@payloadcms.com>
2024-02-09 18:21:29 -0300

    feat: add breadcrumbs nav to hero (#315)
    * Initial commit
    
    * Fix a few blocks' issues and padding
    
    * update components for fixes
    
    * Try a fix
    
    * fix hydration error
    
    * Add comment for the useeffect
    
    * Update spacings for exceptions and update heros
    
    * Update more blocks
    
    * Remove commented code
    
    * make firstContentBlock optional
    
    * fix build
    
    * Change breadcrumsb styling and add breadcrumsb bar above heros
    
    * Update dropdown for mobile
    
    * Update mobile styling
    
    * Update logic on breadcrumbs bar to support all heros
    
    * Fix mobile colours
    
    * Remove comment

---
+++ src/components/Hero/BreadcrumbsBar/index.tsx
@@ -0,0 +1,74 @@
+'use client'
+import React, { useMemo } from 'react'
+
+import { Breadcrumbs } from '@components/Breadcrumbs'
+import { CMSLink } from '@components/CMSLink'
+import { Gutter } from '@components/Gutter'
+import { ChevronIcon } from '@root/icons/ChevronIcon'
+import { Page } from '@root/payload-types'
+
+import classes from './index.module.scss'
+
+interface Props {
+  hero: Page['hero']
+  breadcrumbs: Page['breadcrumbs']
+}
+
+const BreadcrumbsBar: React.FC<Props> = ({ hero, breadcrumbs: breadcrumbsProps }) => {
+  const { breadcrumbsBarLinks, theme } = hero
+
+  const breadcrumbs = useMemo(() => {
+    return breadcrumbsProps?.slice(0, breadcrumbsProps.length - 1) ?? []
+  }, [breadcrumbsProps])
+
+  return (
+    <div
+      className={[classes.wrapper].filter(Boolean).join(' ')}
+      {...(theme ? { 'data-theme': theme } : {})}
+    >
+      <Gutter>
+        <div className={classes.container}>
+          <div>{breadcrumbs.length > 0 && <Breadcrumbs items={breadcrumbs} />}</div>
+
+          <div className={classes.links}>
+            {Array.isArray(breadcrumbsBarLinks) &&
+              breadcrumbsBarLinks.map(({ link }, i) => {
+                return <CMSLink className={classes.link} key={i} {...link} appearance={undefined} />
+              })}
+          </div>
+        </div>
+
+        <div className={classes.containerMobile}>
+          <details className={classes.dropdown}>
+            <summary>
+              {breadcrumbsProps?.[breadcrumbsProps.length - 1].label}{' '}
+              <ChevronIcon className={classes.icon} />{' '}
+            </summary>
+            <div className={classes.dropdownContent}>
+              {Array.isArray(breadcrumbs) &&
+                breadcrumbs.map(({ url, label }, i) => {
+                  return (
+                    <CMSLink
+                      className={classes.link}
+                      key={i}
+                      url={url}
+                      label={label}
+                      appearance={undefined}
+                    />
+                  )
+                })}
+              {Array.isArray(breadcrumbsBarLinks) &&
+                breadcrumbsBarLinks.map(({ link }, i) => {
+                  return (
+                    <CMSLink className={classes.link} key={i} {...link} appearance={undefined} />
+                  )
+                })}
+            </div>
+          </details>
+        </div>
+      </Gutter>
+    </div>
+  )
+}
+
+export default BreadcrumbsBar
