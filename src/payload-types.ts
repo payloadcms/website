1afd1dbf5719d08d316e123d176648fee0d991f6
Paul <paul@payloadcms.com>
2024-02-15 19:23:40 -0300

    feat: adds new callout block ui (#326)
    * Add initial callout block
    
    * Update callout stylings
    
    * updates to new design
    
    * adjust padding
    
    * update settings
    
    * update image position styles
    
    * adjust spacing

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -901,6 +901,28 @@ export interface Page {
   };
   layout: (
     | {
+        calloutFields: {
+          settings?: {
+            theme?: ('light' | 'dark') | null;
+          };
+          richText: {
+            [k: string]: unknown;
+          }[];
+          logo: string | Media;
+          author?: string | null;
+          role?: string | null;
+          images?:
+            | {
+                image: string | Media;
+                id?: string | null;
+              }[]
+            | null;
+        };
+        id?: string | null;
+        blockName?: string | null;
+        blockType: 'callout';
+      }
+    | {
         ctaFields: {
           settings?: {
             theme?: ('light' | 'dark') | null;
@@ -1871,6 +1893,28 @@ export interface ReusableContent {
         id?: string | null;
         blockName?: string | null;
         blockType: 'blogMarkdown';
+      }
+    | {
+        calloutFields: {
+          settings?: {
+            theme?: ('light' | 'dark') | null;
+          };
+          richText: {
+            [k: string]: unknown;
+          }[];
+          logo: string | Media;
+          author?: string | null;
+          role?: string | null;
+          images?:
+            | {
+                image: string | Media;
+                id?: string | null;
+              }[]
+            | null;
+        };
+        id?: string | null;
+        blockName?: string | null;
+        blockType: 'callout';
       }
     | {
         ctaFields: {
