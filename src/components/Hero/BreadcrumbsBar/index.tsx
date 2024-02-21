699281459286b21f3e7535450f8660d8e1902f41
Paul <paul@payloadcms.com>
2024-02-21 19:19:05 -0300

    fix: breadcrumbs bar and adds permanent top bar (#335)
    * fixes breadcrumbs bar and adds permanent top bar
    
    * remove commented code
    
    * Fix home hero padding
    
    * fixes issues
    
    * remove top bar calc
    
    * home hero fixes
    
    * fix

--- src/components/Hero/BreadcrumbsBar/index.tsx
+++ src/components/Hero/BreadcrumbsBar/index.tsx
@@ -1,11 +1,14 @@
 'use client'
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
 import { Breadcrumbs } from '@components/Breadcrumbs'
+import { ChangeHeaderTheme } from '@components/ChangeHeaderTheme'
 import { CMSLink } from '@components/CMSLink'
 import { Gutter } from '@components/Gutter'
 import { ChevronIcon } from '@root/icons/ChevronIcon'
 import { Page } from '@root/payload-types'
+import { useThemePreference } from '@root/providers/Theme'
+import { Theme } from '@root/providers/Theme/types'
 
 import classes from './index.module.scss'
 
@@ -15,59 +18,94 @@ interface Props {
 }
 
 const BreadcrumbsBar: React.FC<Props> = ({ hero, breadcrumbs: breadcrumbsProps }) => {
-  const { breadcrumbsBarLinks, theme } = hero
+  const { breadcrumbsBarLinks, theme, enableBreadcrumbsBar, type } = hero
+  const { theme: themeFromContext } = useThemePreference()
+  const [themeState, setThemeState] = useState<Page['hero']['theme']>(theme)
+
+  const hasBackground = !Boolean(['home'].includes(type))
+
+  useEffect(() => {
+    if (!hasBackground) {
+      setThemeState('dark')
+    } else {
+      if (theme) setThemeState(theme)
+      else if (themeFromContext) setThemeState(themeFromContext)
+    }
+  }, [themeFromContext, theme])
 
   const breadcrumbs = useMemo(() => {
     return breadcrumbsProps?.slice(0, breadcrumbsProps.length - 1) ?? []
   }, [breadcrumbsProps])
 
   return (
-    <div
-      className={[classes.wrapper].filter(Boolean).join(' ')}
-      {...(theme ? { 'data-theme': theme } : {})}
-    >
-      <Gutter>
-        <div className={classes.container}>
-          <div>{breadcrumbs.length > 0 && <Breadcrumbs items={breadcrumbs} />}</div>
+    <ChangeHeaderTheme theme={themeState ?? !hasBackground ? 'dark' : 'light'}>
+      <div
+        className={[classes.wrapper, hasBackground && classes.hasBackground]
+          .filter(Boolean)
+          .join(' ')}
+        {...(themeState ? { 'data-theme': themeState } : {})}
+      >
+        <Gutter>
+          {enableBreadcrumbsBar ? (
+            <>
+              <div className={classes.container}>
+                <div>{breadcrumbs.length > 0 && <Breadcrumbs items={breadcrumbs} />}</div>
 
-          <div className={classes.links}>
-            {Array.isArray(breadcrumbsBarLinks) &&
-              breadcrumbsBarLinks.map(({ link }, i) => {
-                return <CMSLink className={classes.link} key={i} {...link} appearance={undefined} />
-              })}
-          </div>
-        </div>
+                <div className={classes.links}>
+                  {Array.isArray(breadcrumbsBarLinks) &&
+                    breadcrumbsBarLinks.map(({ link }, i) => {
+                      return (
+                        <CMSLink
+                          className={classes.link}
+                          key={i}
+                          {...link}
+                          appearance={undefined}
+                        />
+                      )
+                    })}
+                </div>
+              </div>
 
-        <div className={classes.containerMobile}>
-          <details className={classes.dropdown}>
-            <summary>
-              {breadcrumbsProps?.[breadcrumbsProps.length - 1].label}{' '}
-              <ChevronIcon className={classes.icon} />{' '}
-            </summary>
-            <div className={classes.dropdownContent}>
-              {Array.isArray(breadcrumbs) &&
-                breadcrumbs.map(({ url, label }, i) => {
-                  return (
-                    <CMSLink
-                      className={classes.link}
-                      key={i}
-                      url={url}
-                      label={label}
-                      appearance={undefined}
-                    />
-                  )
-                })}
-              {Array.isArray(breadcrumbsBarLinks) &&
-                breadcrumbsBarLinks.map(({ link }, i) => {
-                  return (
-                    <CMSLink className={classes.link} key={i} {...link} appearance={undefined} />
-                  )
-                })}
-            </div>
-          </details>
-        </div>
-      </Gutter>
-    </div>
+              <div className={classes.containerMobile}>
+                <details className={classes.dropdown}>
+                  <summary>
+                    {breadcrumbsProps?.[breadcrumbsProps.length - 1].label}{' '}
+                    <ChevronIcon className={classes.icon} />{' '}
+                  </summary>
+                  <div className={classes.dropdownContent}>
+                    {Array.isArray(breadcrumbs) &&
+                      breadcrumbs.map(({ url, label }, i) => {
+                        return (
+                          <CMSLink
+                            className={classes.link}
+                            key={i}
+                            url={url}
+                            label={label}
+                            appearance={undefined}
+                          />
+                        )
+                      })}
+                    {Array.isArray(breadcrumbsBarLinks) &&
+                      breadcrumbsBarLinks.map(({ link }, i) => {
+                        return (
+                          <CMSLink
+                            className={classes.link}
+                            key={i}
+                            {...link}
+                            appearance={undefined}
+                          />
+                        )
+                      })}
+                  </div>
+                </details>
+              </div>
+            </>
+          ) : (
+            <div className={classes.emptyBar} />
+          )}
+        </Gutter>
+      </div>
+    </ChangeHeaderTheme>
   )
 }
 
