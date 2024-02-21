3d37bac03c029fbd8a1b28ed15496c474c34ef36
Jessica Chowdhury <jessica@trbl.design>
2024-02-21 14:54:40 -0500

    feat: updates blog pages to new design

--- src/blocks/BlogMarkdown/Block.tsx
+++ src/blocks/BlogMarkdown/Block.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGFM from 'remark-gfm'
 
-import { Gutter } from '@components/Gutter'
 import Table from '@components/MDX/components/Table'
 import { ReusableContent } from '@root/payload-types'
 
@@ -15,19 +14,7 @@ const remarkPlugins = [remarkGFM]
 type Props = Extract<ReusableContent['layout'][0], { blockType: 'blogMarkdown' }>
 
 const BlogMarkdown: React.FC<Props> = ({ blogMarkdownFields: { markdown } }) => {
-  return (
-    <Gutter>
-      <div className={'grid'}>
-        <div className={'cols-8 start-5 cols-m-6 start-m-2 cols-s-8 start-s-1'}>
-          <ReactMarkdown
-            children={markdown}
-            remarkPlugins={remarkPlugins}
-            components={components}
-          />
-        </div>
-      </div>
-    </Gutter>
-  )
+  return <ReactMarkdown children={markdown} remarkPlugins={remarkPlugins} components={components} />
 }
 
 export default BlogMarkdown
