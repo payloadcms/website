060d3e5bce93bfccdf4cf1eb3c417d8bf471b805
Paul Popus <paul@nouance.io>
2024-02-29 14:20:03 -0300

    feat: update styling on blog archive page

--- src/app/(pages)/blog/renderBlogArchive.tsx
+++ src/app/(pages)/blog/renderBlogArchive.tsx
@@ -2,48 +2,57 @@
 
 import React from 'react'
 
+import { BackgroundGrid } from '@components/BackgroundGrid'
 import { BlockSpacing } from '@components/BlockSpacing'
+import { BlockWrapper } from '@components/BlockWrapper'
 import { ContentMediaCard } from '@components/cards/ContentMediaCard'
+import { FeaturedBlogPost } from '@components/FeaturedBlogPost'
 import { Gutter } from '@components/Gutter'
-import { DefaultHero } from '@components/Hero/Default'
 import { Post } from '@root/payload-types'
 
+import classes from './index.module.scss'
+
 export const RenderBlogArchive: React.FC<{ posts: Post[] }> = ({ posts }) => {
+  const latestPost = posts[0]
   return (
     <React.Fragment>
-      <DefaultHero
-        richText={[
-          {
-            type: 'h2',
-            children: [
-              {
-                text: 'Keep tabs on Payload.',
-              },
-            ],
-          },
-          {
-            text: 'Here, you’ll find news about feature releases, happenings in the industry, and Payload announcements in general.',
-          },
-        ]}
-      />
-      <Gutter>
-        <BlockSpacing>
-          <div className={['grid'].filter(Boolean).join(' ')}>
-            {(posts || []).map(blogPost => {
+      <BlockWrapper settings={{}} padding={{ top: 'hero', bottom: 'large' }}>
+        <BackgroundGrid zIndex={0} />
+        <Gutter>
+          <div className={[classes.hero].filter(Boolean).join(' ')}>
+            <div>
+              <h1 className={[classes.pageTitle].filter(Boolean).join(' ')}>Blog</h1>
+            </div>
+            <div className={[classes.heroContent, 'grid'].filter(Boolean).join(' ')}>
+              <h2 className={[classes.title, 'cols-8 cols-m-8'].filter(Boolean).join(' ')}>
+                Keep tabs on Payload
+              </h2>
+              <p
+                className={[classes.description, 'cols-4 start-13 start-m-1 cols-m-8']
+                  .filter(Boolean)
+                  .join(' ')}
+              >{`Here, you’ll find news about feature releases, happenings in the industry, and Payload announcements in general.`}</p>
+            </div>
+          </div>
+
+          <FeaturedBlogPost {...latestPost} />
+          <div className={[classes.cardGrid, 'grid'].filter(Boolean).join(' ')}>
+            {(posts || []).slice(1).map(blogPost => {
               return (
-                <div key={blogPost.id} className={['cols-4 cols-s-8'].filter(Boolean).join(' ')}>
+                <div key={blogPost.id} className={['cols-8 cols-m-8'].filter(Boolean).join(' ')}>
                   <ContentMediaCard
                     title={blogPost.title}
-                    description={blogPost?.meta?.description}
+                    publishedOn={blogPost.publishedOn}
                     href={`/blog/${blogPost.slug}`}
                     media={blogPost.image}
+                    authors={blogPost.authors}
                   />
                 </div>
               )
             })}
           </div>
-        </BlockSpacing>
-      </Gutter>
+        </Gutter>
+      </BlockWrapper>
     </React.Fragment>
   )
 }
