3e41585bb7074b0c06fb11d24b48c3cd1484aabf
Patrik Kozak <patrik@payloadcms.com>
2024-03-01 11:15:04 -0500

    chore: updates code feature block with new alignment field

--- src/blocks/CodeFeature/index.tsx
+++ src/blocks/CodeFeature/index.tsx
@@ -29,10 +29,13 @@ export const CodeFeatureComponent: React.FC<Props> = ({
   const [tabWrapperWidth, setTabWrapperWidth] = useState(0)
   const tabWrapperRef = useRef<HTMLDivElement>(null)
   const activeTabRef = useRef<HTMLButtonElement>(null)
-  const { heading, richText, codeTabs, links, settings } = codeFeatureFields
+  const { alignment, heading, richText, codeTabs, links, settings } = codeFeatureFields
   const hasLinks = Boolean(links?.length && links.length > 0)
   const id = useId()
   const { data, isOpen } = CodeBlip.useCodeBlip()
+
+  const tabsWrapperRef = useRef<HTMLDivElement | null>(null)
+  const [backgroundHeight, setBackgroundHeight] = useState(0)
 
   useEffect(() => {
     let observer
@@ -91,6 +94,17 @@ export const CodeFeatureComponent: React.FC<Props> = ({
     }
   }, [activeIndex])
 
+  useEffect(() => {
+    const updateBackgroundHeight = () => {
+      const newVideoHeight = tabsWrapperRef.current ? tabsWrapperRef.current.offsetHeight : 0
+      setBackgroundHeight(newVideoHeight)
+    }
+    updateBackgroundHeight()
+    window.addEventListener('resize', updateBackgroundHeight)
+
+    return () => window.removeEventListener('resize', updateBackgroundHeight)
+  }, [])
+
   return (
     <BlockWrapper
       settings={settings}
@@ -100,119 +114,259 @@ export const CodeFeatureComponent: React.FC<Props> = ({
     >
       <BackgroundGrid zIndex={0} className={classes.backgroundGrid} />
       <Gutter>
-        <div
-          className={[classes.container, hasLinks && classes.hasLinks, 'grid']
-            .filter(Boolean)
-            .join(' ')}
-        >
-          <div className={[classes.scanlineWrapper, 'start-9 cols-8'].filter(Boolean).join(' ')}>
-            <BackgroundScanline
-              className={[classes.scanlineDesktop].filter(Boolean).join(' ')}
-              crosshairs={['top-left', 'bottom-left']}
-            />
+        {alignment === 'codeContent' ? (
+          <div
+            className={[classes.container, hasLinks && classes.hasLinks, 'grid']
+              .filter(Boolean)
+              .join(' ')}
+          >
+            <div
+              className={[
+                classes.scanlineWrapper,
+                alignment === 'codeContent' ? classes.scanlineWrapperLeft : '',
+                'start-1 cols-8 start-m-1 cols-m-8',
+              ]
+                .filter(Boolean)
+                .join(' ')}
+              style={{ height: `calc(${backgroundHeight}px + var(--base) * 10)` }}
+            >
+              <BackgroundScanline
+                className={[classes.scanlineDesktopLeft].filter(Boolean).join(' ')}
+                crosshairs={['top-right', 'bottom-right']}
+              />
 
-            <CrosshairIcon className={[classes.crosshairTopRight].filter(Boolean).join(' ')} />
+              <CrosshairIcon className={[classes.crosshairTopLeft].filter(Boolean).join(' ')} />
 
-            <CrosshairIcon className={[classes.crosshairBottomRight].filter(Boolean).join(' ')} />
-          </div>
-          <div className={[classes.content, 'cols-4 cols-m-8'].filter(Boolean).join(' ')}>
-            {heading && (
-              <h2 className={classes.heading}>
-                <SplitAnimate text={heading} />
-              </h2>
-            )}
-            <div>
-              <RichText content={richText} className={classes.richText} />
-              <div className={classes.links}>
-                {links?.map((link, index) => {
+              <CrosshairIcon className={[classes.crosshairBottomLeft].filter(Boolean).join(' ')} />
+            </div>
+            <div
+              className={[classes.content, 'start-13 cols-4 cols-m-8 start-m-1']
+                .filter(Boolean)
+                .join(' ')}
+            >
+              {heading && (
+                <h2 className={classes.heading}>
+                  <SplitAnimate text={heading} />
+                </h2>
+              )}
+              <div>
+                <RichText content={richText} className={classes.richText} />
+                <div className={classes.links}>
+                  {links?.map((link, index) => {
+                    return (
+                      <CMSLink
+                        key={index}
+                        appearance={'default'}
+                        buttonProps={{
+                          appearance: 'default',
+                          hideHorizontalBorders: true,
+                          hideBottomBorderExceptLast: true,
+                        }}
+                        {...link.link}
+                      />
+                    )
+                  })}
+                </div>
+              </div>
+            </div>
+            <div
+              ref={tabsWrapperRef}
+              className={[classes.tabsWrapper, 'cols-10 start-1 cols-m-8 start-m-1']
+                .filter(Boolean)
+                .join(' ')}
+            >
+              <BackgroundScanline
+                className={[classes.scanlineMobile, ''].filter(Boolean).join(' ')}
+              />
+              <CodeBlip.Modal />
+              <div
+                className={[
+                  classes.tabs,
+                  codeTabs?.length && codeTabs.length > 1 && classes.hasMultiple,
+                ]
+                  .filter(Boolean)
+                  .join(' ')}
+                ref={tabWrapperRef}
+                {...(isOpen ? { inert: '' } : {})}
+              >
+                {codeTabs?.length && codeTabs.length > 1 ? (
+                  codeTabs?.map((code, index) => {
+                    const isActive = activeIndex === index
+                    return (
+                      <button
+                        key={index}
+                        className={[classes.tab, activeIndex === index && classes.isActive]
+                          .filter(Boolean)
+                          .join(' ')}
+                        onClick={() => setActiveIndex(index)}
+                        aria-pressed={activeIndex === index}
+                        id={`codefeature${id}-tab-${index}`}
+                        aria-controls={`codefeature${id}-code-${index}`}
+                        {...(isActive ? { ref: activeTabRef } : {})}
+                      >
+                        {code?.label}
+                      </button>
+                    )
+                  })
+                ) : (
+                  <div className={classes.hiddenTab} id={`codefeature${id}-tab-0`}>
+                    {codeTabs?.[0]?.label}
+                  </div>
+                )}
+                <div className={classes.tabIndicator} style={indicatorStyle} aria-hidden={true} />
+              </div>
+              <div className={classes.codeBlockWrapper} {...(isOpen ? { inert: '' } : {})}>
+                {codeTabs?.map((code, index) => {
                   return (
-                    <CMSLink
+                    <div
                       key={index}
-                      appearance={'default'}
-                      buttonProps={{
-                        appearance: 'default',
-                        hideHorizontalBorders: true,
-                        hideBottomBorderExceptLast: true,
-                      }}
-                      {...link.link}
-                    />
+                      className={[
+                        classes.codeBlock,
+                        activeIndex === index && classes.isActive,
+                        activeIndex === index && 'group-active',
+                      ]
+                        .filter(Boolean)
+                        .join(' ')}
+                      aria-hidden={activeIndex !== index}
+                      aria-describedby={`codefeature${id}-tab-${index}`}
+                      id={`codefeature${id}-code-${index}`}
+                      // types have not been updated yet for the inert attribute
+                      // @ts-expect-error
+                      inert={activeIndex !== index ? '' : undefined}
+                    >
+                      <Code parentClassName={classes.code} codeBlips={code.codeBlips}>{`${code.code}
+                  `}</Code>
+                    </div>
                   )
                 })}
               </div>
             </div>
           </div>
+        ) : (
           <div
-            className={[classes.tabsWrapper, 'cols-10 start-7 cols-m-8 start-m-1']
+            className={[classes.container, hasLinks && classes.hasLinks, 'grid']
               .filter(Boolean)
               .join(' ')}
           >
-            <BackgroundScanline
-              className={[classes.scanlineMobile, ''].filter(Boolean).join(' ')}
-            />
-            <CodeBlip.Modal />
             <div
-              className={[
-                classes.tabs,
-                codeTabs?.length && codeTabs.length > 1 && classes.hasMultiple,
-              ]
+              className={[classes.content, 'start-1 cols-4 cols-m-8 start-m-1']
                 .filter(Boolean)
                 .join(' ')}
-              ref={tabWrapperRef}
-              {...(isOpen ? { inert: '' } : {})}
             >
-              {codeTabs?.length && codeTabs.length > 1 ? (
-                codeTabs?.map((code, index) => {
-                  const isActive = activeIndex === index
+              {heading && (
+                <h2 className={classes.heading}>
+                  <SplitAnimate text={heading} />
+                </h2>
+              )}
+              <div>
+                <RichText content={richText} className={classes.richText} />
+                <div className={classes.links}>
+                  {links?.map((link, index) => {
+                    return (
+                      <CMSLink
+                        key={index}
+                        appearance={'default'}
+                        buttonProps={{
+                          appearance: 'default',
+                          hideHorizontalBorders: true,
+                          hideBottomBorderExceptLast: true,
+                        }}
+                        {...link.link}
+                      />
+                    )
+                  })}
+                </div>
+              </div>
+            </div>
+            <div
+              className={[classes.scanlineWrapper, 'start-9 cols-8 start-m-1 cols-m-8']
+                .filter(Boolean)
+                .join(' ')}
+              style={{ height: `calc(${backgroundHeight}px + var(--base) * 10)` }}
+            >
+              <BackgroundScanline
+                className={[classes.scanlineDesktopRight].filter(Boolean).join(' ')}
+                crosshairs={['top-left', 'bottom-left']}
+              />
+
+              <CrosshairIcon className={[classes.crosshairTopRight].filter(Boolean).join(' ')} />
+
+              <CrosshairIcon className={[classes.crosshairBottomRight].filter(Boolean).join(' ')} />
+            </div>
+            <div
+              ref={tabsWrapperRef}
+              className={[classes.tabsWrapper, 'cols-10 start-7 cols-m-8 start-m-1']
+                .filter(Boolean)
+                .join(' ')}
+            >
+              <BackgroundScanline
+                className={[classes.scanlineMobile, ''].filter(Boolean).join(' ')}
+              />
+              <CodeBlip.Modal />
+              <div
+                className={[
+                  classes.tabs,
+                  codeTabs?.length && codeTabs.length > 1 && classes.hasMultiple,
+                ]
+                  .filter(Boolean)
+                  .join(' ')}
+                ref={tabWrapperRef}
+                {...(isOpen ? { inert: '' } : {})}
+              >
+                {codeTabs?.length && codeTabs.length > 1 ? (
+                  codeTabs?.map((code, index) => {
+                    const isActive = activeIndex === index
+                    return (
+                      <button
+                        key={index}
+                        className={[classes.tab, activeIndex === index && classes.isActive]
+                          .filter(Boolean)
+                          .join(' ')}
+                        onClick={() => setActiveIndex(index)}
+                        aria-pressed={activeIndex === index}
+                        id={`codefeature${id}-tab-${index}`}
+                        aria-controls={`codefeature${id}-code-${index}`}
+                        {...(isActive ? { ref: activeTabRef } : {})}
+                      >
+                        {code?.label}
+                      </button>
+                    )
+                  })
+                ) : (
+                  <div className={classes.hiddenTab} id={`codefeature${id}-tab-0`}>
+                    {codeTabs?.[0]?.label}
+                  </div>
+                )}
+                <div className={classes.tabIndicator} style={indicatorStyle} aria-hidden={true} />
+              </div>
+              <div className={classes.codeBlockWrapper} {...(isOpen ? { inert: '' } : {})}>
+                {codeTabs?.map((code, index) => {
                   return (
-                    <button
+                    <div
                       key={index}
-                      className={[classes.tab, activeIndex === index && classes.isActive]
+                      className={[
+                        classes.codeBlock,
+                        activeIndex === index && classes.isActive,
+                        activeIndex === index && 'group-active',
+                      ]
                         .filter(Boolean)
                         .join(' ')}
-                      onClick={() => setActiveIndex(index)}
-                      aria-pressed={activeIndex === index}
-                      id={`codefeature${id}-tab-${index}`}
-                      aria-controls={`codefeature${id}-code-${index}`}
-                      {...(isActive ? { ref: activeTabRef } : {})}
+                      aria-hidden={activeIndex !== index}
+                      aria-describedby={`codefeature${id}-tab-${index}`}
+                      id={`codefeature${id}-code-${index}`}
+                      // types have not been updated yet for the inert attribute
+                      // @ts-expect-error
+                      inert={activeIndex !== index ? '' : undefined}
                     >
-                      {code?.label}
-                    </button>
-                  )
-                })
-              ) : (
-                <div className={classes.hiddenTab} id={`codefeature${id}-tab-0`}>
-                  {codeTabs?.[0]?.label}
-                </div>
-              )}
-              <div className={classes.tabIndicator} style={indicatorStyle} aria-hidden={true} />
-            </div>
-            <div className={classes.codeBlockWrapper} {...(isOpen ? { inert: '' } : {})}>
-              {codeTabs?.map((code, index) => {
-                return (
-                  <div
-                    key={index}
-                    className={[
-                      classes.codeBlock,
-                      activeIndex === index && classes.isActive,
-                      activeIndex === index && 'group-active',
-                    ]
-                      .filter(Boolean)
-                      .join(' ')}
-                    aria-hidden={activeIndex !== index}
-                    aria-describedby={`codefeature${id}-tab-${index}`}
-                    id={`codefeature${id}-code-${index}`}
-                    // types have not been updated yet for the inert attribute
-                    // @ts-expect-error
-                    inert={activeIndex !== index ? '' : undefined}
-                  >
-                    <Code parentClassName={classes.code} codeBlips={code.codeBlips}>{`${code.code}
+                      <Code parentClassName={classes.code} codeBlips={code.codeBlips}>{`${code.code}
                   `}</Code>
-                  </div>
-                )
-              })}
+                    </div>
+                  )
+                })}
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </Gutter>
     </BlockWrapper>
   )
