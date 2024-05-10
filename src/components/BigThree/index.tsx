e2f75d18fe98ea5947524ca5336b56d4ab1f38f3
Tylan Davis <hello@tylandavis.com>
2024-05-10 09:17:20 -0400

    chore: styling on 3.0 hero option

--- src/components/BigThree/index.tsx
+++ src/components/BigThree/index.tsx
@@ -1,101 +1,20 @@
-'use client'
-import React, { CSSProperties, useEffect, useRef, useState } from 'react'
-import { motion, MotionValue } from 'framer-motion'
-
-import { BackgroundScanline } from '@components/BackgroundScanline'
-
 import classes from './index.module.scss'
 
 interface BigThreeProps {
   className?: string
-  whiteOpacity?: MotionValue
-  style?: CSSProperties
 }
 
 const BigThree: React.FC<BigThreeProps> = props => {
-  const { className, whiteOpacity } = props
-  const containerRef = useRef<HTMLDivElement>(null)
-  const defaultStyles = {
-    '--mouse-x': 0,
-    '--mouse-y': 0,
-  } as CSSProperties
-  const [gradientStyles, setGradientStyle] = useState<CSSProperties>(defaultStyles)
-
-  useEffect(() => {
-    let intersectionObserver: IntersectionObserver
-    let scheduledAnimationFrame = false
-
-    const updateMousePosition = e => {
-      if (containerRef.current) {
-        const boundingRect = containerRef.current.getBoundingClientRect()
-        const x = e.clientX - boundingRect.left
-        const y = e.clientY - boundingRect.top
-
-        const styles = {
-          '--mouse-x': x,
-          '--mouse-y': y,
-        } as CSSProperties
-
-        setGradientStyle(styles)
-      }
-      scheduledAnimationFrame = false
-    }
-
-    const handleMouseMovement = e => {
-      if (scheduledAnimationFrame) {
-        return
-      }
-
-      scheduledAnimationFrame = true
-      requestAnimationFrame(function (timestamp) {
-        updateMousePosition(e)
-      })
-    }
-
-    if (containerRef.current) {
-      intersectionObserver = new IntersectionObserver(
-        entries => {
-          entries.forEach(entry => {
-            if (entry.isIntersecting) {
-              window.addEventListener('mousemove', handleMouseMovement)
-            } else {
-              window.removeEventListener('mousemove', handleMouseMovement)
-            }
-          })
-        },
-        {
-          rootMargin: '0px',
-        },
-      )
-
-      intersectionObserver.observe(containerRef.current)
-    }
-
-    return () => {
-      if (intersectionObserver) intersectionObserver.disconnect()
-      window.removeEventListener('mousemove', handleMouseMovement)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [containerRef])
+  const { className } = props
 
   return (
-    <div
-      ref={containerRef}
-      className={[className, classes.container].filter(Boolean).join(' ')}
-      data-theme="dark"
-    >
-      <div className={classes.mask}>
-        <div className={classes.gradient} style={gradientStyles} />
-        <div className={classes.noise} />
-        <div className={classes.black} />
-      </div>
+    <div className={[className, classes.container].filter(Boolean).join(' ')} data-theme="dark">
       <svg
-        width="1601"
-        height="855"
+        width="1200"
         viewBox="0 0 1601 855"
-        fill="none"
+        fill="white"
         xmlns="http://www.w3.org/2000/svg"
-        className={classes.stroke}
+        className={classes.three}
       >
         <path d="M628.256 616.221C628.256 744.592 518.389 854.458 322.942 854.458C134.433 854.458 14.1582 750.374 0.280273 594.248H147.155C154.094 680.984 221.17 737.653 322.942 737.653C424.713 737.653 486.007 686.767 486.007 605.813C486.007 522.545 422.4 482.068 328.724 482.068H243.144V368.731H326.411C403.896 368.731 466.347 334.037 466.347 246.143C466.347 174.441 417.774 117.773 326.411 117.773C229.266 117.773 172.598 182.536 166.815 265.804H25.7231C34.9751 122.398 139.059 0.966716 327.568 0.966716C513.763 0.966716 609.752 114.303 609.752 241.517C609.752 338.663 544.988 408.052 461.721 423.087C561.179 439.278 628.256 514.45 628.256 616.221Z" />
         <path d="M691.655 840.581V668.263H863.973V840.581H691.655Z" />
--- src/components/Hero/Three/index.tsx
+++ src/components/Hero/Three/index.tsx
@@ -1,15 +1,10 @@
-'use client'
-
-import React, { useRef } from 'react'
-import { cubicBezier, motion, useScroll, useTransform } from 'framer-motion'
-
+import BackgroundGradient from '@components/BackgroundGradient'
 import { BackgroundGrid } from '@components/BackgroundGrid'
 import BigThree from '@components/BigThree'
 import { BlockWrapper } from '@components/BlockWrapper'
 import { CMSLink } from '@components/CMSLink'
 import CreatePayloadApp from '@components/CreatePayloadApp'
 import { Gutter } from '@components/Gutter'
-import { useGetHeroPadding } from '@components/Hero/useGetHeroPadding'
 import { BlocksProp } from '@components/RenderBlocks'
 import { RichText } from '@components/RichText'
 import { Page } from '@root/payload-types'
@@ -22,66 +17,54 @@ export const ThreeHero: React.FC<
     firstContentBlock?: BlocksProp
   }
 > = ({ richText, buttons, theme, firstContentBlock }) => {
-  const padding = useGetHeroPadding(theme, firstContentBlock)
-  const scrollRef = useRef(null)
-  const { scrollYProgress } = useScroll({
-    target: scrollRef,
-    offset: ['start 90px', 'end start'],
-  })
-  const y = useTransform(scrollYProgress, [0, 1], ['translateY(-55%)', 'translateY(-200%)'])
-
   return (
-    <BlockWrapper settings={{ theme }} padding={padding} className={classes.blockWrapper}>
-      <BackgroundGrid zIndex={0} />
-      <Gutter>
-        <div className={[classes.wrapper, 'grid'].filter(Boolean).join(' ')}>
-          <div
-            className={[classes.sidebar, `cols-4`, 'cols-m-8 start-1'].filter(Boolean).join(' ')}
-          >
-            <RichText content={richText} className={[classes.richText].filter(Boolean).join(' ')} />
+    <>
+      <BlockWrapper settings={{ theme }} className={classes.blockWrapper}>
+        <BackgroundGrid zIndex={1} />
+        <Gutter>
+          <div className={[classes.wrapper, 'grid'].filter(Boolean).join(' ')}>
+            <div className={[classes.sidebar, 'cols-4 cols-m-8 start-1'].filter(Boolean).join(' ')}>
+              <RichText
+                content={richText}
+                className={[classes.richText].filter(Boolean).join(' ')}
+              />
 
-            <div className={classes.linksWrapper}>
-              {Array.isArray(buttons) &&
-                buttons.map((button, i) => {
-                  if (button.blockType === 'command') {
-                    return (
-                      <CreatePayloadApp
-                        key={i + button.command}
-                        label={button.command}
-                        background={false}
-                        className={classes.createPayloadApp}
-                      />
-                    )
-                  }
-                  if (button.blockType === 'link' && button.link) {
-                    return (
-                      <CMSLink
-                        key={i + button.link.label}
-                        {...button.link}
-                        className={classes.link}
-                        appearance="default"
-                        buttonProps={{
-                          hideBorders: true,
-                        }}
-                      />
-                    )
-                  }
-                })}
+              <div className={classes.linksWrapper}>
+                {Array.isArray(buttons) &&
+                  buttons.map((button, i) => {
+                    if (button.blockType === 'command') {
+                      return (
+                        <CreatePayloadApp
+                          key={i + button.command}
+                          label={button.command}
+                          background={false}
+                          className={classes.createPayloadApp}
+                        />
+                      )
+                    }
+                    if (button.blockType === 'link' && button.link) {
+                      return (
+                        <CMSLink
+                          key={i + button.link.label}
+                          {...button.link}
+                          className={classes.link}
+                          appearance="default"
+                          buttonProps={{
+                            hideBorders: true,
+                          }}
+                        />
+                      )
+                    }
+                  })}
+              </div>
+            </div>
+            <div className={[classes.graphicWrapper, 'cols-8 start-8'].join(' ')}>
+              <BigThree />
             </div>
           </div>
-          <div className="gradients" />
-          <motion.div
-            className={classes.graphicWrapper}
-            style={{ transform: y, zIndex: 2 }}
-            initial={{ opacity: 0, marginTop: '150vh' }}
-            animate={{ opacity: 1, marginTop: '0px' }}
-            transition={{ duration: 2, ease: cubicBezier(0.25, 0, 0, 1) }}
-          >
-            <BigThree />
-          </motion.div>
-        </div>
-        <div className={classes.defaultHero}></div>
-      </Gutter>
-    </BlockWrapper>
+        </Gutter>
+      </BlockWrapper>
+      <BackgroundGradient className={classes.backgroundGradient} />
+    </>
   )
 }
