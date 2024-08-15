8abe9fb7a6bc8359ec4867a3cf59b6e50bf1dc52
Tylan Davis <hello@tylandavis.com>
2024-08-15 15:11:49 -0400

    chore: improve three hero responsiveness

--- src/components/MediaStack/index.tsx
+++ src/components/MediaStack/index.tsx
@@ -11,13 +11,13 @@ type MediaStackProps = {
 
 export const MediaStack: React.FC<MediaStackProps> = ({ media }) => {
   return (
-    <>
+    <div className={classes.stack}>
       {typeof media[0].image !== 'string' && (
         <Media resource={media[0].image} className={classes.mediaOne} />
       )}
       {typeof media[1].image !== 'string' && (
         <Media resource={media[1].image} className={classes.mediaTwo} />
       )}
-    </>
+    </div>
   )
 }
