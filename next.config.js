e7781772f0653162e2d2e216fb9e0d46167d15e6
Tylan Davis <hello@tylandavis.com>
2024-09-09 13:59:34 -0400

    update local remotePatterns

--- next.config.js
+++ next.config.js
@@ -24,6 +24,11 @@ const localhost = process.env.NEXT_PUBLIC_IS_LIVE
       },
       {
         protocol: 'http',
+        hostname: 'local.payloadcms.com',
+        port: '3000',
+      },
+      {
+        protocol: 'http',
         hostname: 'cms.local.payloadcms.com',
         port: '8000',
       },
@@ -131,34 +136,36 @@ const nextConfig = withBundleAnalyzer({
 
 // Injected content via Sentry wizard below
 
-export default withPayload(withSentryConfig(
-  nextConfig,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
+export default withPayload(
+  withSentryConfig(
+    nextConfig,
+    {
+      // For all available options, see:
+      // https://github.com/getsentry/sentry-webpack-plugin#options
 
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: 'payload-cms',
-    project: 'website',
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+      // Suppresses source map uploading logs during build
+      silent: true,
+      org: 'payload-cms',
+      project: 'website',
+    },
+    {
+      // For all available options, see:
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: true,
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
+      // Transpiles SDK to be compatible with IE11 (increases bundle size)
+      transpileClientSDK: true,
 
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: '/monitoring',
+      // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+      tunnelRoute: '/monitoring',
 
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
+      // Hides source maps from generated client bundles
+      hideSourceMaps: true,
 
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  },
-))
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      disableLogger: true,
+    },
+  ),
+)
