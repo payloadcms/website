1443e5eda01e85791fc92b2469a97746368a7243
Tylan Davis <hello@tylandavis.com>
2024-09-23 11:08:24 -0400

    remove dynamic redirects from nextConfig

--- redirects.js
+++ redirects.js
@@ -32,53 +32,7 @@ export const redirects = async () => {
     destination: '/ie-incompatible.html',
   }
 
-  const redirectsRes = await fetch(
-    `${process.env.NEXT_PUBLIC_CMS_URL}/api/redirects?limit=1000&depth=1`,
-  )
-  const redirectsData = await redirectsRes.json()
-
-  const { docs } = redirectsData
-
-  let dynamicRedirects = []
-
-  if (docs) {
-    docs.forEach(doc => {
-      const { from, to: { type, url, reference } = {} } = doc
-
-      let source = from.replace(process.env.NEXT_PUBLIC_SITE_URL, '').split('?')[0].toLowerCase()
-
-      if (source.endsWith('/')) source = source.slice(0, -1) // a trailing slash will break this redirect
-
-      let destination = '/'
-
-      if (type === 'custom' && url) {
-        destination = url.replace(process.env.NEXT_PUBLIC_SITE_URL, '')
-      }
-
-      if (
-        type === 'reference' &&
-        typeof reference.value === 'object' &&
-        reference?.value?._status === 'published'
-      ) {
-        destination = `${process.env.NEXT_PUBLIC_SITE_URL}/${formatPermalink(reference)}`
-      }
-
-      const redirect = {
-        source,
-        destination,
-        permanent: true,
-      }
-
-      if (source.startsWith('/') && destination && source !== destination) {
-        return dynamicRedirects.push(redirect)
-      }
-
-      return
-    })
-  }
-
-  const redirects = [...staticRedirects, internetExplorerRedirect, ...dynamicRedirects]
-  // const redirects = []
+  const redirects = [...staticRedirects, internetExplorerRedirect]
 
   return redirects
 }
