3430868acf81cde5a935e9cbc667c0b73f0e599e
Paul Popus <paul@nouance.io>
2024-10-24 18:23:33 -0600

    fix: docs anchors when they're inlinecodes as headings being incorrectly formatted

--- src/components/MDX/components/Headings/formatAnchor.ts
+++ src/components/MDX/components/Headings/formatAnchor.ts
@@ -1,7 +1,26 @@
+function extractChildren(node: any): string {
+  if (node.props && node.props.children) {
+    if (typeof node.props.children === 'string') {
+      return node.props.children
+    } else if (typeof node.props.children === 'object') {
+      return extractChildren(node.props.children)
+    }
+  }
+  return ''
+}
+
 const flattenChildren: (value: string | { props: { children: any } }) => string = value => {
   if (typeof value === 'string') {
     return value
   }
+
+  // Extract a deeply nested string from a Lexical node
+  const stringValue = extractChildren(value)
+
+  if (typeof stringValue === 'string') {
+    return stringValue
+  }
+
   return value.props.children
 }
 
