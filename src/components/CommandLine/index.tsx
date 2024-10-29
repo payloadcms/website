ef245544233a8b845f7e635b1b2e242b512cce58
Tylan Davis <hello@tylandavis.com>
2024-10-29 09:23:36 -0400

    add command line component

---
+++ src/components/CommandLine/index.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import { CheckIcon, CopyIcon } from '@payloadcms/ui'
+import React from 'react'
+
+import classes from './index.module.scss'
+
+export const CommandLine = ({ command }: { command: string }) => {
+  const [copied, setCopied] = React.useState(false)
+  const CopyToClipboard = async (command: string) => {
+    await navigator.clipboard.writeText(command).then(() => {
+      setCopied(true)
+      setTimeout(() => {
+        setCopied(false)
+      }, 2000)
+    })
+  }
+
+  return (
+    <button
+      aria-label={`Copy command: ${command}`}
+      aria-live="polite"
+      className={classes.commandLine}
+      onClick={() => CopyToClipboard(command)}
+      type="button"
+    >
+      <span aria-hidden="true" className={classes.commandText}>
+        $&nbsp;&nbsp;{command}
+      </span>
+      <div className={classes.icon}>
+        {copied ? <CheckIcon aria-label="Copied" /> : <CopyIcon aria-label="Copy to clipboard" />}
+      </div>
+    </button>
+  )
+}
