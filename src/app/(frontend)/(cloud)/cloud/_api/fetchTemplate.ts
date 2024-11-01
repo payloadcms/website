5cae93ee7f31e619119c5f371403c73706e6aa57
Elliot DeNolf <denolfe@users.noreply.github.com>
2024-11-01 15:49:40 -0400

    feat(templates): include credentials when querying templates (#389)

--- src/app/(frontend)/(cloud)/cloud/_api/fetchTemplate.ts
+++ src/app/(frontend)/(cloud)/cloud/_api/fetchTemplate.ts
@@ -1,18 +1,20 @@
-import { TEMPLATE } from '@data/templates.js'
 import type { Template } from '@root/payload-cloud-types.js'
+
+import { TEMPLATE } from '@data/templates.js'
 
 export const fetchTemplate = async (templateSlug?: string): Promise<Template> => {
   const doc: Template = await fetch(`${process.env.NEXT_PUBLIC_CLOUD_CMS_URL}/api/graphql`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify({
       query: TEMPLATE,
       variables: {
         slug: templateSlug,
       },
     }),
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
   })
     ?.then(res => res.json())
     ?.then(res => {
