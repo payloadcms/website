5d55cd494e8eafc1a99d40b14eacd149cde67254
Jessica Chowdhury <jessica@trbl.design>
2025-01-07 17:46:14 +0000

    chore: update algolia env var names

--- src/collections/CommunityHelp/updateAlgolia.ts
+++ src/collections/CommunityHelp/updateAlgolia.ts
@@ -1,8 +1,8 @@
 import algoliasearch from 'algoliasearch'
 
-const appID = process.env.ALGOLIA_CH_ID || ''
-const apiKey = process.env.ALGOLIA_API_KEY || ''
-const indexName = process.env.ALGOLIA_CH_INDEX_NAME || ''
+const appID = process.env.NEXT_PUBLIC_ALGOLIA_CH_ID || ''
+const apiKey = process.env.NEXT_PRIVATE_ALGOLIA_API_KEY || ''
+const indexName = process.env.NEXT_PUBLIC_ALGOLIA_CH_INDEX_NAME || ''
 
 const client = algoliasearch(appID, apiKey)
 
