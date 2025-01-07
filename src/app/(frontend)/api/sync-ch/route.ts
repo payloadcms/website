9545b0b8f4f6b38df3edbf5126273c873da13ff1
Alessio Gravili <alessio@gravili.de>
2025-01-06 20:05:28 -0700

    fix: negligence

--- src/app/(frontend)/api/sync-ch/route.ts
+++ src/app/(frontend)/api/sync-ch/route.ts
@@ -4,7 +4,7 @@ import fetchDiscord from '../../../../scripts/fetchDiscord'
 import fetchGitHub from '../../../../scripts/fetchGitHub'
 import syncToAlgolia from '../../../../scripts/syncToAlgolia'
 
-export const maxDuration = 60 * 15 // 15 mins
+export const maxDuration = 900 // 15 mins
 export const dynamic = 'force-dynamic'
 
 export async function GET(): Promise<NextResponse> {
