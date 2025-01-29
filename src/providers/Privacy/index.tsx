02f9cffe11af6edda085140c43ca182f3949a3ae
Tylan Davis <hello@tylandavis.com>
2025-01-29 11:24:07 -0500

    simplify cookie consent logic

--- src/providers/Privacy/index.tsx
+++ src/providers/Privacy/index.tsx
@@ -3,15 +3,11 @@
 import canUseDom from '@root/utilities/can-use-dom.js'
 import React, { createContext, useCallback, useContext, useEffect, useState } from 'react'
 
-import type { LocateResponse } from '../../../functions-api.js'
-
-import { locate } from '../../../functions-api.js'
-
 type Privacy = {
   cookieConsent?: boolean
   country?: string
   showConsent?: boolean
-  updateCookieConsent: (accepted: boolean, rejected: boolean) => void
+  updateCookieConsent: (accepted: boolean) => void
 }
 
 const Context = createContext<Privacy>({
@@ -25,44 +21,29 @@ type CookieConsent = {
   accepted: boolean
   at: string
   country: string
-  rejected: boolean
 }
 
-const getLocaleStorage = (): CookieConsent =>
-  canUseDom && JSON.parse(window.localStorage.getItem('cookieConsent') || 'null')
-const setLocaleStorage = (accepted: boolean, rejected: boolean, country: string) => {
+const getLocaleStorage = (): CookieConsent | null =>
+  canUseDom ? JSON.parse(window.localStorage.getItem('cookieConsent') || 'null') : null
+
+const setLocaleStorage = (accepted: boolean, country: string) => {
   const cookieConsent: CookieConsent = {
     accepted,
     at: new Date().toISOString(),
     country,
-    rejected,
   }
   window.localStorage.setItem('cookieConsent', JSON.stringify(cookieConsent))
 }
 
-const getGDPR = async (): Promise<LocateResponse> => {
-  const res = await locate()
-  if (res.status === 200) {
-    const result: LocateResponse = await res.json()
-    return result
-  }
-  return { isGDPR: true }
-}
-
-type PrivacyProviderProps = {
-  children: React.ReactNode
-}
-
-const PrivacyProvider: React.FC<PrivacyProviderProps> = (props) => {
-  const { children } = props
+const PrivacyProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [showConsent, setShowConsent] = useState<boolean | undefined>()
   const [cookieConsent, setCookieConsent] = useState<boolean | undefined>()
   const [country, setCountry] = useState<string | undefined>()
 
   const updateCookieConsent = useCallback(
-    (accepted: boolean, rejected: boolean) => {
+    (accepted: boolean) => {
       setCookieConsent(accepted)
-      setLocaleStorage(accepted, rejected, country || '')
+      setLocaleStorage(accepted, country || '')
     },
     [country],
   )
@@ -70,21 +51,20 @@ const PrivacyProvider: React.FC<PrivacyProviderProps> = (props) => {
   useEffect(() => {
     ;(async () => {
       const consent = getLocaleStorage()
-      if (consent !== null) {
-        setCountry(consent?.country)
-        setCookieConsent(consent.accepted)
+      if (consent) {
+        setCountry(consent.country)
+        setCookieConsent(consent.accepted || false)
         return
       }
-      const gdpr = await getGDPR()
-      if (gdpr.country) {
-        setCountry(gdpr.country)
-      }
+      const gdpr = await fetch('/api/locate').then((res) => res.json())
+
+      setCountry(gdpr.country || '')
       if (!gdpr.isGDPR) {
         setCookieConsent(true)
-        updateCookieConsent(true, false)
+        updateCookieConsent(true)
       }
-      setShowConsent(gdpr?.isGDPR || false)
-    })()
+      setShowConsent(gdpr.isGDPR || false)
+    })().catch(console.error)
   }, [updateCookieConsent])
 
   useEffect(() => {
@@ -97,17 +77,11 @@ const PrivacyProvider: React.FC<PrivacyProviderProps> = (props) => {
           ReactPixel.revokeConsent()
         }
       })
+      .catch(console.error)
   }, [cookieConsent])
 
   return (
-    <Context.Provider
-      value={{
-        cookieConsent,
-        country,
-        showConsent,
-        updateCookieConsent,
-      }}
-    >
+    <Context.Provider value={{ cookieConsent, country, showConsent, updateCookieConsent }}>
       {children}
     </Context.Provider>
   )
