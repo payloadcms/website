af85c7f69bd88b17790c3ac4adc1130a99dff465
Tylan Davis <hello@tylandavis.com>
2025-02-12 11:27:03 -0500

    fix: broken full youtube links with timestamps

--- src/utilities/get-video.ts
+++ src/utilities/get-video.ts
@@ -5,7 +5,7 @@ export const getVideo: (videoUrl: string) => {
 } = (videoUrl: string) => {
   return {
     id: videoUrl.includes('youtube')
-      ? videoUrl.split('v=')[1]
+      ? videoUrl.split('v=')[1].split('&')[0] || ''
       : videoUrl.split('/').pop()?.split('?')[0] || '',
     platform: videoUrl.includes('vimeo') ? 'vimeo' : 'youtube',
     start: videoUrl.includes('t=') ? parseInt(videoUrl.split('t=').pop() || '0') : 0,
