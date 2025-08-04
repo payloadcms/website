163e22db6caf777c44f89f17cd600950afab0c99
Tylan Davis <tylan@payloadcms.com>
2025-08-04 14:34:58 -0600

    fix: broken titles on Docs relationship field

--- src/collections/Docs/index.ts
+++ src/collections/Docs/index.ts
@@ -219,6 +219,11 @@ export const Docs: CollectionConfig = {
       },
     },
   ],
+  forceSelect: {
+    slug: true,
+    topic: true,
+    version: true,
+  },
   hooks: {
     afterRead: [
       async ({ doc: _doc, findMany, req }) => {
