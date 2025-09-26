a59b0aa0d931cec2d6d7307db63fa3c9ab31c2d1
Tylan Davis <tylan@payloadcms.com>
2025-09-26 12:31:22 -0600

    fix: open in new tab not working on Top Bar links

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -6,13 +6,13 @@ import { ArrowIcon } from '@icons/ArrowIcon'
 import classes from './index.module.scss'
 
 export const TopBar: React.FC<TopBarType> = ({ link, message }) => {
-  const { type, label, reference, url } = link || {}
-
   return (
-    <CMSLink className={classes.topBar} reference={reference} type={type} url={url}>
-      <span className={classes.message}>{message}</span>
-      <span className={classes.label}>{label}</span>
-      <ArrowIcon />
-    </CMSLink>
+    link && (
+      <CMSLink className={classes.topBar} {...link} label={undefined}>
+        <span className={classes.message}>{message}</span>
+        <span className={classes.label}>{link.label}</span>
+        <ArrowIcon />
+      </CMSLink>
+    )
   )
 }
