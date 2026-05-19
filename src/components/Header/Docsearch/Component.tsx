6f1af1cef39e756e8826e4cdb2caa19dcc04e5ee
Maria Juliana Arias Hernandez <mariasher0310@gmail.com>
2026-05-19 00:40:10 -0700

    fix(search): filter search results by docs version and show version badge

--- src/components/Header/Docsearch/Component.tsx
+++ src/components/Header/Docsearch/Component.tsx
@@ -4,14 +4,25 @@ import React from 'react'
 
 import classes from './index.module.scss'
 
-function Hit({ children, hit, path }) {
-  const blog = hit?.url?.includes('/blog/') || false
+function getVersionedUrl(url: string, path: string): string {
+  if (!url) return url
 
-  let url = hit?.url
+  const isV2 = path.includes('/docs/v2/')
 
-  if (path.includes('/docs/v2/')) {
-    url = url.replace('/docs/', '/docs/v2/')
+  if (isV2 && !url.includes('/docs/v2/')) {
+    return url.replace('/docs/', '/docs/v2/')
   }
+
+  if (!isV2 && url.includes('/docs/v2/')) {
+    return url.replace('/docs/v2/', '/docs/')
+  }
+
+  return url
+}
+
+function Hit({ children, hit, path }: { children: React.ReactNode; hit: any; path: string }) {
+  const blog = hit?.url?.includes('/blog/') || false
+  const url = getVersionedUrl(hit?.url, path)
 
   return (
     <a className={blog ? classes.blogResult : ''} href={url}>
@@ -22,14 +33,29 @@ function Hit({ children, hit, path }) {
 
 function Component() {
   const path = usePathname()
+  const isV2 = path.includes('/docs/v2/')
   return (
+    <div className={classes.searchWrapper}> 
     <DocSearch
       apiKey={process.env.NEXT_PUBLIC_ALGOLIA_DOCSEARCH_KEY || ''}
       appId="9MJY7K9GOW"
       hitComponent={({ children, hit }) => Hit({ children, hit, path })}
       indexName="payloadcms"
+      searchParameters={
+        isV2
+          ? { facetFilters: ['version:v2'] }
+          : { facetFilters: ['version:v3'] }
+      }
     />
+    {isV2 && (
+      <div className={classes.versionBadge}>
+        Searching in v2 docs
+      </div>
+    )}
+    </div>
   )
 }
+
+
 
 export default Component
