8dba79f73451ac4bc097034c187f0c671936c169
Patrik Kozak <35232443+PatrikKozak@users.noreply.github.com>
2026-07-31 12:39:38 -0400

    chore: bump payload packages to 3.87.0

--- src/collections/Docs/mdxToLexical.ts
+++ src/collections/Docs/mdxToLexical.ts
@@ -36,7 +36,7 @@ export const UploadBlockMarkdownTransformer: ElementTransformer = {
 
     const captionText =
       caption && hasText(caption)
-        ? `\n${lexicalToMDX({ editorConfig: cachedServerEditorConfig as any, editorState: caption })}`
+        ? `\n${lexicalToMDX({ editorConfig: cachedServerEditorConfig as any, editorState: caption as DefaultTypedEditorState<SerializedBlockNode> })}`
         : ''
 
     if (altText?.length) {
